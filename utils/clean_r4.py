75dff9b3456bba392cec824dab101e82e6966af9
simondaout <simondaout@gmail.com>
2018-10-29 18:08:32 +0100

    clean and plot .r4 files

---
+++ utils/clean_r4.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+################################################################################
+# Author        : Simon DAOUT 
+################################################################################
+
+"""\
+clean_r4.py
+-------------
+Clean a r4 file given an other r4 file (mask) and a threshold on this mask
+
+Usage: clean_r4.py --infile=<path> --mask=<path> --threshold=<value> --outfile=<path> [--lectfile=<path>] [--scale=<value>] 
+
+Options:
+-h --help           Show this screen.
+--infile PATH       r4 file to clean
+--outfile PATH      output file
+--mask PATH         r4 file used as mask
+--threshold VALUE   threshold value on mask file (Keep pixel with mask > threshold)
+--scale VALUE       scale the mask [default:1]
+--lectfile PATH     Path of the lect.in file [default: lect.in]
+"""
+
+# numpy
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
+
+
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+from pylab import *
+
+from nsbas import docopt
+arguments = docopt.docopt(__doc__)
+infile = arguments["--infile"]
+outfile = arguments["--outfile"]
+maskf = arguments["--mask"]
+seuil = float(arguments["--threshold"])
+if arguments["--lectfile"] ==  None:
+   lecfile = "lect.in"
+else:
+   lecfile = arguments["--lectfile"]
+if arguments["--scale"] ==  None:
+   scale = 1.
+else:
+   scale = float(arguments["--scale"])
+
+# read lect.in 
+ncol, nlign = map(int, open(lecfile).readline().split(None, 2)[0:2])
+fid = open(infile, 'r')
+m = np.fromfile(fid,dtype=np.float32).reshape((nlign,ncol))
+# m[m==0.0] = np.float('NaN')
+
+perc=99.9
+maxlos,minlos=np.nanpercentile(m,perc),np.nanpercentile(m,(100-perc))
+print maxlos,minlos
+kk = np.nonzero(
+    np.logical_or(m<minlos,m>maxlos))
+m[kk] = np.float('NaN')
+
+# mask
+fid2 = open(maskf, 'r')
+mask = np.fromfile(fid2,dtype=np.float32).reshape((nlign,ncol))
+mask =  mask*scale
+kk = np.nonzero(np.logical_or(mask==0, mask>seuil))
+mf = np.copy(m)
+mf[kk] = np.float('NaN')
+#plt.imshow(mf)
+#plt.show()
+#sys.exit()
+
+#save output
+fid3 = open(outfile,'wb')
+mf.flatten().astype('float32').tofile(fid3)
+
+# Plot
+vmax = np.nanmean(mf) + 2*np.nanstd(mf)
+vmin = np.nanmean(mf) - 2*np.nanstd(mf)
+
+fig = plt.figure(0,figsize=(12,8))
+ax = fig.add_subplot(1,3,1)
+# hax = ax.imshow(mask, cm.Greys, vmin=0, vmax=seuil)
+cax = ax.imshow(m, cm.jet,vmin=vmin, vmax=vmax)
+ax.set_title(infile)
+setp( ax.get_xticklabels(), visible=False)
+cbar = fig.colorbar(cax, orientation='vertical',aspect=9)
+
+ax = fig.add_subplot(1,3,2)
+cax = ax.imshow(mask, cm.jet, vmin=0, vmax=seuil)
+ax.set_title(maskf)
+setp( ax.get_xticklabels(), visible=False)
+cbar = fig.colorbar(cax, orientation='vertical',aspect=9)
+
+ax = fig.add_subplot(1,3,3)
+cax = ax.imshow(mf, cm.jet, vmin=vmin, vmax=vmax)
+setp( ax.get_xticklabels(), visible=False)
+setp( ax.get_xticklabels(), visible=False)
+cbar = fig.colorbar(cax, orientation='vertical',aspect=9)
+ax.set_title(outfile)
+
+fig.tight_layout()
+plt.show()
---
+++ utils/plot_r4.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+################################################################################
+# 
+# NSBAS - New Small Baseline Chain
+# 
+################################################################################
+# Author        : Simon DAOUT (ISTerre)
+################################################################################
+
+"""\
+plot_r4.py
+-------------
+
+Usage: plot_r4.py --infile=<path> [--lectfile=<path>] [--vmin=<value>] [--vmax=<value>] [--wrap=<yes/no>] [--crop=<values>]
+
+Options:
+-h --help           Show this screen.
+--infile PATH       File to be cut
+--lectfile PATH     Path of the lect.in file [default: lect.in]
+--vmax 				Max colorscale [default: 90th percentile]
+--vmin 				Min colorscale [default: 10th percentile]
+--wrap 				Wrapped phase [default: no]
+--crop VALUE        Define a region of interest [default: 0,nlign,0,ncol]
+
+"""
+
+# numpy
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
+
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+from pylab import *
+
+# docopt (command line parser)
+from nsbas import docopt
+
+# read arguments
+arguments = docopt.docopt(__doc__)
+infile = arguments["--infile"]
+
+if arguments["--lectfile"] ==  None:
+    lecfile = "lect.in"
+else:
+    lecfile = arguments["--lectfile"]
+
+# read lect.in 
+ncol, nlign = map(int, open(lecfile).readline().split(None, 2)[0:2])
+
+if arguments["--crop"] ==  None:
+    crop = [0,nlign,0,ncol]
+else:
+    crop = map(float,arguments["--crop"].replace(',',' ').split())
+ibeg,iend,jbeg,jend = int(crop[0]),int(crop[1]),int(crop[2]),int(crop[3])
+
+# Program
+fid = open(infile, 'r')
+m = np.fromfile(fid,dtype=np.float32).reshape((nlign,ncol))[ibeg:iend,jbeg:jend]
+kk = np.nonzero(np.logical_or(np.logical_or(~np.isnan(m), np.abs(m)<999.),m==0.0))
+mprim = m[kk]
+
+if arguments["--vmax"] ==  None:
+	vmax = np.nanpercentile(mprim, 98)
+else:
+	vmax = np.float(arguments["--vmax"])
+
+if arguments["--vmin"] ==  None:
+	vmin = np.nanpercentile(mprim, 2)
+else:
+	vmin = np.float(arguments["--vmin"])
+print vmax,vmin
+
+if arguments["--wrap"] !=  None:	
+	m = np.mod(m,2*np.pi)-np.pi 
+	vmax=np.pi
+	vmin=-np.pi
+
+# Plot
+fig = plt.figure(0,figsize=(6,4))
+ax = fig.add_subplot(1,1,1)
+cax = ax.imshow(m,cmap=cm.jet,vmax=vmax,vmin=vmin)
+ax.set_title(infile)
+setp( ax.get_xticklabels(), visible=False)
+cbar = fig.colorbar(cax, orientation='vertical',aspect=5)
+
+fig.savefig('{}.eps'.format(infile), format='EPS',dpi=150)
+plt.show()
