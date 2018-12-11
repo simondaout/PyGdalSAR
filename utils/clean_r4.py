53dea665bdfe624e4d4049320aa8ff2b26f245f8
simondaout <simondaout@gmail.com>
2018-12-11 17:04:44 +0000

    add crop option

--- utils/clean_r4.py
+++ utils/clean_r4.py
@@ -14,7 +14,7 @@ clean_r4.py
 -------------
 Clean a r4 file given an other r4 file (mask) and a threshold on this mask
 
-Usage: clean_r4.py --infile=<path> --mask=<path> --threshold=<value> --outfile=<path> [--lectfile=<path>] [--scale=<value>] 
+Usage: clean_r4.py --infile=<path> --mask=<path> --threshold=<value> --outfile=<path> [--perc=<value>] [--clean=<values>] [--lectfile=<path>] [--scale=<value>] 
 
 Options:
 -h --help           Show this screen.
@@ -24,6 +24,8 @@ Options:
 --threshold VALUE   threshold value on mask file (Keep pixel with mask > threshold)
 --scale VALUE       scale the mask [default:1]
 --lectfile PATH     Path of the lect.in file [default: lect.in]
+--clean VALUE       Clean option [default: 0,ncol,0,nlign]
+--perc VALUE        Percentile of hidden LOS pixel for the estimation and clean outliers [default:99.9]
 """
 
 # numpy
@@ -51,18 +53,30 @@ if arguments["--scale"] ==  None:
 else:
    scale = float(arguments["--scale"])
 
+if arguments["--clean"] ==  None:
+    mask = [0,0,0,0]
+else:
+    print arguments["--clean"]
+    mask = map(float,arguments["--clean"].replace(',',' ').split())
+ibeg,iend,jbeg,jend = int(mask[0]),int(mask[1]),int(mask[2]),int(mask[3])
+
+if arguments["--perc"] ==  None:
+    perc = 99.9
+else:
+    perc = float(arguments["--perc"])
+
 # read lect.in 
 ncol, nlign = map(int, open(lecfile).readline().split(None, 2)[0:2])
 fid = open(infile, 'r')
 m = np.fromfile(fid,dtype=np.float32).reshape((nlign,ncol))
 # m[m==0.0] = np.float('NaN')
 
-perc=99.9
 maxlos,minlos=np.nanpercentile(m,perc),np.nanpercentile(m,(100-perc))
 print maxlos,minlos
 kk = np.nonzero(
     np.logical_or(m<minlos,m>maxlos))
 m[kk] = np.float('NaN')
+
 
 # mask
 fid2 = open(maskf, 'r')
@@ -71,6 +85,60 @@ mask =  mask*scale
 kk = np.nonzero(np.logical_or(mask==0, mask>seuil))
 mf = np.copy(m)
 mf[kk] = np.float('NaN')
+
+# ibeg, iend = 250, 900
+# jbeg, jend = 2000, 2900 
+mcrop = np.nanpercentile(mf[jbeg:jend,ibeg:iend],85)
+
+ibeg1,iend1 = 0, ibeg
+ibeg2,iend2 = iend,ncol
+jbeg1,jend1 = 0, jbeg
+jbeg2,jend2 = jend, nlign
+
+buf = 50
+# top 
+# print mcrop
+for j in xrange(buf):
+  # print jend1+j, (np.float(j+1)/buf)
+  for i in xrange(ibeg,iend):
+	# mf[jend1+j,:] = mcrop*(np.float(j+1)/buf)
+	# print jend1+j, mcrop*(np.float(j+1)/buf)
+    mf[jend1+j,i] = mf[jend1+j,i]*(np.float(j+1)/buf)
+
+#bottom
+# print mcrop
+for j in xrange(buf):
+  for i in xrange(ibeg,iend):
+	# print jbeg2-(buf-j), mcrop - mcrop*(np.float(j+1)/buf)
+	# mf[jbeg2-(buf-j),:] = mcrop - mcrop*(np.float(j+1)/buf)
+	mf[jbeg2-(buf-j),i] = mf[jbeg2-(buf-j),i] - mf[jbeg2-(buf-j),i]*(np.float(j+1)/buf)
+
+# left
+# print mcrop
+for j in xrange(buf):
+  for i in xrange(ibeg,iend):	
+	# print iend1+(buf-j), mcrop - mcrop*(np.float(j+1)/buf)
+	# mf[jbeg+buf:jend-buf,iend1+(buf-j)] = mcrop - mcrop*(np.float(j+1)/buf)
+	mf[i,iend1+(buf-j)] = mf[i,iend1+(buf-j)] - mf[i,iend1+(buf-j)]*(np.float(j+1)/buf)
+
+# right
+# print mcrop
+for j in xrange(buf):
+  for i in xrange(ibeg,iend):
+	# print ibeg2-(buf-j),  mcrop - mcrop*(np.float(j+1)/buf)
+	# mf[jbeg+buf:jend-buf,ibeg2-(buf-j)] = mcrop - mcrop*(np.float(j+1)/buf)
+	mf[i,ibeg2-(buf-j)] = mf[i,ibeg2-(buf-j)] - mf[i,ibeg2-(buf-j)]*(np.float(j+1)/buf)
+
+
+mf[jbeg1:jend1,:] = 0.0
+mf[jbeg2:jend2,:] = 0.0
+mf[:,ibeg1:iend1] = 0.0
+mf[:,ibeg2:iend2] = 0.0
+mf[jbeg1:jend1,ibeg1:iend1] = 0.0
+mf[jbeg2:jend2,ibeg2:iend2] = 0.0
+
+
+
 #plt.imshow(mf)
 #plt.show()
 #sys.exit()
@@ -80,8 +148,8 @@ fid3 = open(outfile,'wb')
 mf.flatten().astype('float32').tofile(fid3)
 
 # Plot
-vmax = np.nanmean(mf) + 2*np.nanstd(mf)
-vmin = np.nanmean(mf) - 2*np.nanstd(mf)
+vmax = np.nanpercentile(m,99) 
+vmin = -vmax
 
 fig = plt.figure(0,figsize=(12,8))
 ax = fig.add_subplot(1,3,1)
