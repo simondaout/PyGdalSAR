aafb9bf5883e725c80b46b71350c5b3025b91fa7
simondaout <simondaout@gmail.com>
2019-02-13 16:51:04 +0000

    correct bug read gamma par file

--- parsers/gamma.py
+++ parsers/gamma.py
@@ -32,8 +32,10 @@ def _getParameters(path, log=False):
     required_slc = ['nlines', 'width']
     required_int = ['range_samples','azimuth_lines']
 
-    path = op.dirname(op.realpath(path))
+    # path = op.dirname(op.realpath(path))
+    # print(path,%s/*par' % path')
     par_files = glob.glob('%s/*par' % path)
+    # print(par_files)
 
     for file in par_files:
         params = _parseParameterFile(file)
@@ -42,7 +44,7 @@ def _getParameters(path, log=False):
         if check_required(required_int, params)\
               or check_required(required_slc, params):
                 if not log:
-                    self._log.info('Found parameter file %s' % file)
+                    print('Found parameter file %s' % file)
                 return params
 
     raise ImportError(
@@ -61,14 +63,14 @@ def readpar(par_file='./'):
         :raises: ImportError
         """
         
-        params = _getParameters(par_file, log=True)
+        params = _getParameters(par_file,log=True)
         nrows = int(params['range_samples'][0]) 
         nlines = int(params['azimuth_lines'][0])
         return nlines,nrows
 
 def readgamma(filename,par_file='./'):
 
-        params = _getParameters(par_file, log=True)
+        params = _getParameters(par_file,log=True)
         nrows = int(params['range_samples'][0]) 
         nlines = int(params['azimuth_lines'][0])
         
