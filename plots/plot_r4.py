df6ea6f4c70c22af886b21e78011f7b5b3c521e8
simondaout <simondaout@gmail.com>
2019-02-15 10:32:27 +0000

    add rad2mm and title option

--- plots/plot_r4.py
+++ plots/plot_r4.py
@@ -13,17 +13,18 @@
 plot_r4.py
 -------------
 
-Usage: plot_r4.py --infile=<path> [--lectfile=<path>] [--vmin=<value>] [--vmax=<value>] [--wrap=<yes/no>] [--crop=<values>]
+Usage: plot_r4.py --infile=<path> [--lectfile=<path>] [--vmin=<value>] [--vmax=<value>] [--wrap=<yes/no>] [--rad2mm=<value>] [--crop=<values>] [--title=<value>]
 
 Options:
--h --help           Show this screen.
---infile PATH       File to be cut
---lectfile PATH     Path of the lect.in file [default: lect.in]
---vmax 				Max colorscale [default: 90th percentile]
---vmin 				Min colorscale [default: 10th percentile]
---wrap 				Wrapped phase [default: no]
---crop VALUE        Define a region of interest [default: 0,nlign,0,ncol]
-
+-h --help             Show this screen.
+--infile=<file>       File to be cut
+--lectfile=<file>     Path of the lect.in file [default: lect.in]
+--vmax=<value> 	      Max colorscale [default: 90th percentile]
+--vmin=<value>        Min colorscale [default: 10th percentile]
+--wrap=<value> 	      Wrapped phase [default: no]
+--rad2mm=<value>      Convert data [default: 1]
+--tile=<value>        Title plot 
+--crop=<value>        Define a region of interest [default: 0,nlign,0,ncol]
 """
 
 # numpy
@@ -41,8 +42,8 @@ import docopt
 
 # read arguments
 arguments = docopt.docopt(__doc__)
-infile = arguments["--infile"]
 
+infile = arguments["--infile"]
 if arguments["--lectfile"] ==  None:
     lecfile = "lect.in"
 else:
@@ -59,7 +60,7 @@ ibeg,iend,jbeg,jend = int(crop[0]),int(crop[1]),int(crop[2]),int(crop[3])
 
 # Program
 fid = open(infile, 'r')
-m = np.fromfile(fid,dtype=np.float32).reshape((nlign,ncol))[ibeg:iend,jbeg:jend]
+m = np.fromfile(fid,dtype=np.float32)[:nlign*ncol].reshape((nlign,ncol))[ibeg:iend,jbeg:jend]
 kk = np.nonzero(np.logical_or(np.logical_or(~np.isnan(m), np.abs(m)<999.),m==0.0))
 mprim = m[kk]
 
@@ -79,11 +80,21 @@ if arguments["--wrap"] !=  None:
 	vmax=np.pi
 	vmin=-np.pi
 
+if arguments["--rad2mm"] ==  None:
+        rad2mm = 1
+else:
+        rad2mm = np.float(arguments["--rad2mm"])
+
 # Plot
+m, vmin, vmax = m*rad2mm, vmin*rad2mm, vmax*rad2mm
+
 fig = plt.figure(0,figsize=(8,10))
 ax = fig.add_subplot(1,1,1)
 cax = ax.imshow(m,cmap=cm.jet,vmax=vmax,vmin=vmin)
-ax.set_title(infile)
+if arguments["--title"] ==  None:
+	ax.set_title(infile)
+else:
+	ax.set_title(arguments["--title"])
 setp( ax.get_xticklabels(), visible=False)
 divider = make_axes_locatable(ax)
 c = divider.append_axes("right", size="5%", pad=0.05)
