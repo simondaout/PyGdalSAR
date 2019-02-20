76ed4b5ee87567f6dd921d64ad62666c97f0584f
simondaout <simondaout@gmail.com>
2019-02-20 16:28:10 +0000

    add docopt

--- utils/correct_ts_from_model.py
+++ utils/correct_ts_from_model.py
@@ -6,6 +6,42 @@
 ################################################################################
 
 
+"""\
+correct_ts_from_model.py
+-------------
+Correct time series from coefficienf file obtained with invers_disp2coef.py
+Input crep maps must be named *_coef_clean.r4  
+
+Usage: correct_ts_from_model.py [--cube=<path>] [--list_images=<path>] [--lectfile=<path>] \
+ [--slope=<path>] [--coseismic=<paths>] [--postseismic=<paths>] [--slowslip=<value>] \
+ [--cos=<path>] [--sin=<path>] [--dem=<path>] [--aps=<path>] \
+ [--rad2mm=<value>] [--imref=<value>] [--crop=<values>]\
+[--dateslim=<values>] [--cols=<values>] [--ligns=<values>] [--bounds=<value>]
+
+
+Options:
+-h --help           Show this screen
+--cube PATH         Path to displacement file [default: depl_cumul_flat]
+--list_images PATH  Path to list images file made of 5 columns containing for each images 1) number 2) Doppler freq (not read) 3) date in YYYYMMDD format 4) numerical date 5) perpendicular baseline [default: images_retenues]
+--lectfile PATH     Path of the lect.in file [default: lect.in]
+--slope PATH        Path to velocity map in r4 or tif format (e.g lin_coeff.r4) [default: None]
+--cos PATH          Path to cosinus map in r4 or tif format (e.g coswt_coeff.r4) [default: None]
+--sin PATH          Path to sinus map in r4 or tif format (e.g sinwt_coeff.r4)  [default: None]
+--coseismic         Time of the events (e.g 2013.2,2014.5) [default: None]
+--postseismic       Characteristic time of the transient functions (e.g 1.,0.5) [default: None]
+--slowslip   VALUE  Read slow-slip maps. Indicate median and characteristic time of the events
+--dem PATH          Path to dem file error map in r4 or tif format (demerr_coeff.r4) [default: None]
+--aps PATH          Path to aps file: 1 column file given aps for each dates (eg. aps.txt) [default: None]
+--rad2mm            Scaling value between input data (rad) and desired output [default: -4.4563]
+--imref VALUE       Reference image number [default: 1]
+--cols VALUE        Pixels column numbers to display (eg. 200,400,450) 
+--ligns VALUE       Pixels lign numbers pixel to display (eg. 1200,1200,3000)
+--crop VALUE        Crop option [default: 0,nlign,0,ncol] 
+--bounds            yMin,yMax time series plots
+"""
+
+
+
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
 import matplotlib.pyplot as plt
@@ -16,138 +52,310 @@ import math
 import sys
 import matplotlib.dates as mdates
 import datetime
+import os
 
+###############################################################
+# define basis functioGns for plot
 def Heaviside(t):
-        h=np.zeros((len(t)))
-        h[t>=0]=1.0
-        return h
+    h=np.zeros((len(t)))
+    h[t>=0]=1.0
+    return h
 
-def Box(t):
-        return Heaviside(t+0.5)-Heaviside(t-0.5)
+def coseismic(time,to,step):
+    return step*Heaviside(time-to)
 
-class postseismic():
-    def __init__(self,date,tcar=1.):
-        self.to=date
-        self.tcar=tcar
+def postseismic(time,to,tcar,trans):
+       t=(time-to)/tcar
+       t[t<=0] = 0
+       g = np.log10(1+t)
+       return trans*g
 
-    def g(self,t):
-        t=(t-self.to)/self.tcar
-        t[t<=0] = 0
-        g = np.log10(1+t)
-        return g
+def linear(time,v):
+    return v*(time-datemin)
 
-class coseismic():
-      def __init__(self,date):
-          self.to=date
+def seasonal(time,a,b):
+    return a*np.cos(2*np.pi*(time-datemin)) + b*np.sin(2*np.pi*(time-datemin))
 
-      def g(self,t):
-        return Heaviside(t-self.to)
+def slowslip(time,to,tcar):
+          t=(time-to)/tcar
+          funct = 0.5*(np.tanh(t)-1) + 1
+          return funct
+###############################################################
+
+# docopt (command line parser)
+import docopt
+
+# read arguments
+arguments = docopt.docopt(__doc__)
+if arguments["--list_images"] ==  None:
+    listim = "images_retenues"
+else:
+    listim = arguments["--list_images"]
+if arguments["--cube"] ==  None:
+   cubef = "depl_cumule_flat"
+else:
+   cubef = arguments["--cube"]
+if arguments["--lectfile"] ==  None:
+   infile = "lect.in"
+else:
+   infile = arguments["--lectfile"]
+if arguments["--slope"] ==  None:
+   slopef = None
+else:
+   slopef = arguments["--slope"]
+
+if arguments["--cos"] ==  None:
+   cosf = None
+else:
+   cosf = arguments["--cos"]
+if arguments["--sin"] ==  None:
+   sinf = None
+else:
+   sinf = arguments["--sin"]
+if sinf is None and cosf is not None or cosf is None and sinf is not None:
+    raise Exception("cosinus and sinus files must be given")
+
+if arguments["--coseismic"] ==  None:
+   cotimes = []
+else:
+   cotimes = map(float,arguments["--coseismic"].replace(',',' ').split())
+if arguments["--postseismic"] ==  None:
+   postimes = []
+else:
+   postimes = map(float,arguments["--postseismic"].replace('None','-1').replace(',',' ').split())
+
+if len(postimes)>0 and len(cotimes) != len(postimes):
+    raise Exception("coseimic and postseismic lists are not the same size")
+if arguments["--dem"] ==  None:
+   demf = None
+else:
+   demf = arguments["--dem"]
+if arguments["--rad2mm"] ==  None:
+    rad2mm = -56/(4*np.pi)
+else:
+    rad2mm = float(arguments["--rad2mm"]) 
+if arguments["--imref"] ==  None:
+    imref = 0
+elif arguments["--imref"] < 1:
+    print '--imref must be between 1 and Nimages'
+else:
+    imref = int(arguments["--imref"]) - 1
+
+if arguments["--bounds"] is not  None:
+    ylim = map(float,arguments["--bounds"].replace(',',' ').split())
+
+if arguments["--slowslip"] == None:
+    sse=[]
+else:
+    sse = map(float,arguments["--slowslip"].replace(',',' ').split()) 
+sse_time = sse[::2]
+sse_car = sse[1::2]  
+
+###############################################################
+
+# create a list of pixels
+jpix = map(int,arguments["--cols"].replace(',',' ').split())
+ipix = map(int,arguments["--ligns"].replace(',',' ').split())
+if len(jpix) != len(ipix):
+    raise Exception("ncols and nligns lists are not the same size")
+# number of pixels
+Npix = len(ipix)
 
 # load images_retenues file
-nb,idates,dates,base=np.loadtxt('images_retenues', comments='#', usecols=(0,1,3,5), unpack=True,dtype='i,i,f,f')
-N=len(dates)
-datemin, datemax = np.int(np.min(dates)), np.int(np.max(dates))+1
+# load images_retenues file
+nb,idates,tdec,base=np.loadtxt(listim, comments='#', usecols=(0,1,3,5), unpack=True,dtype='i,i,f,f')
+datemin, datemax = np.int(np.min(tdec)), np.int(np.max(tdec))+1
+N = len(idates)
+base = base - base[imref]
 print 'Number images: ', N
-# read lect.in
-ncol, nlign = map(int, open('lect_ts.in').readline().split(None, 2)[0:2])
+# read lect.in 
+ncol, nlign = map(int, open(infile).readline().split(None, 2)[0:2])
+
 # lect cube
-cube = np.fromfile('depl_cumule_flat',dtype=np.float32)
-maps = cube[:nlign*ncol*N].reshape((nlign,ncol,N))
+maps = np.fromfile(cubef,dtype=np.float32)[:nlign*ncol*N].reshape((nlign,ncol,N))
 
-# compute model
-co0 = 2003.29
-tcar=.3
-co1 = 2004.36
-infile = 'post0_coeff_clean.r4'
-post0 = np.fromfile(infile,dtype=np.float32).reshape((nlign,ncol))
-infile = 'cos0_coeff_clean.r4'
-cos0 = np.fromfile(infile,dtype=np.float32).reshape((nlign,ncol))
-#infile = 'cos1_coeff_clean.r4'
-#cos1 = np.fromfile(infile,dtype=np.float32).reshape((nlign,ncol))
-infile = 'ref_coeff.r4'
-ref = np.fromfile(infile,dtype=np.float32).reshape((nlign,ncol))
+if arguments["--crop"] ==  None:
+    crop = [0,nlign,0,ncol]
+else:
+    crop = map(float,arguments["--crop"].replace(',',' ').split())
+ibeg,iend,jbeg,jend = int(crop[0]),int(crop[1]),int(crop[2]),int(crop[3])
+    
+if slopef is not None:
+  extension = os.path.splitext(slopef)[1]
+  if extension == ".tif":
+    ds = gdal.Open(slopef, gdal.GA_ReadOnly)
+    slopemap = ds.GetRasterBand(1).ReadAsArray()
+  else:
+    slopemap = np.fromfile(slopef,dtype=np.float32).reshape((nlign,ncol))
+else:
+    slopemap = np.zeros((nlign,ncol))
 
-# crop
-# ibeg,iend = 2180,2640
-# jbeg,jend = 340,800
+try:
+  ds = gdal.Open('ref_coeff.tif', gdal.GA_ReadOnly)
+  band = ds.GetRasterBand(1)
+  refmap =  band.ReadAsArray()
+except:
+  refmap = np.fromfile('ref_coeff.r4',dtype=np.float32).reshape((nlign,ncol))
 
-ibeg,iend = 0,nlign
-jbeg,jend = 0,ncol
+if demf is not None:
+  extension = os.path.splitext(demf)[1]
+  if extension == ".tif":
+    ds = gdal.Open(demf, gdal.GA_ReadOnly)
+    demmap = ds.GetRasterBand(1).ReadAsArray()
+  else:
+    demmap = np.fromfile(demf,dtype=np.float32).reshape((nlign,ncol))   
+else:
+    demmap = np.zeros((nlign,ncol))
 
-cos1=np.zeros((nlign,ncol))
+if cosf is not None:
+  extension = os.path.splitext(cosf)[1]
+  if extension == ".tif":
+    ds = gdal.Open(cosf, gdal.GA_ReadOnly)
+    cosmap = ds.GetRasterBand(1).ReadAsArray()
+  else:
+    cosmap = np.fromfile(cosf,dtype=np.float32).reshape((nlign,ncol))   
 
+if sinf is not None:
+  extension = os.path.splitext(sinf)[1]
+  if extension == ".tif":
+    ds = gdal.Open(sinf, gdal.GA_ReadOnly)
+    sinmap = ds.GetRasterBand(1).ReadAsArray()
+  else:
+    sinmap = np.fromfile(sinf,dtype=np.float32).reshape((nlign,ncol))
+
+M = len(cotimes)
+coseismaps=np.zeros((M,nlign,ncol))
+postmaps=np.zeros((M,nlign,ncol))
+# if no postseismic functions defined then set all functions to 0
+if len(postimes) == 0:
+    postimes = np.ones((M))*-1
+
+for i in xrange(M):
+  cofile = 'cos{}_coeff_clean.r4'.format(i)
+  try:
+    ds = gdal.Open(cofile, gdal.GA_ReadOnly)
+    coseismaps[i,:,:] = ds.GetRasterBand(1).ReadAsArray()
+  except:
+    coseismaps[i,:,:] = np.fromfile(cofile,dtype=np.float32).reshape((nlign,ncol))
+
+for i in xrange((M)):
+  postfile = 'post{}_coeff_clean.r4'.format(i)
+  if postimes[i] > 0:
+    try:
+      ds = gdal.Open(postfile, gdal.GA_ReadOnly)
+      postmaps[i,:,:] = ds.GetRasterBand(1).ReadAsArray()
+    except:  
+      postmaps[i,:,:] = np.fromfile(postfile,dtype=np.float32).reshape((nlign,ncol))
+  else: 
+      postmaps[i,:,:] = np.zeros((nlign,ncol))
+  
+sse_times = sse[::2]
+sse_car = sse[1::2] 
+L = len(sse_times)
+ssemaps=np.zeros((M,nlign,ncol))
+for i in xrange(L):
+    try:
+      ds = gdal.Open('sse{}_coeff_clean.tif'.format(i), gdal.GA_ReadOnly)
+      ssemaps[i,:,:] = ds.GetRasterBand(1).ReadAsArray()
+    except:
+      ssemaps[i,:,:] = np.fromfile('sse{}_coeff_clean.r4'.format(i),dtype=np.float32).reshape((nlign,ncol))
+
+
+###############################################################
+# figure ts
+fig = plt.figure(0,figsize=(10,9))
+fig.subplots_adjust(wspace=0.001)
+
+# extract model
+k = 0
 model = np.zeros((nlign,ncol,N))
-for i in xrange(ibeg,iend):
-    for j in xrange(jbeg,jend):
-        # model[i,j,:] = ref[i,j]
-        model[i,j,:] = cos0[i,j]*coseismic(co0).g(dates) +post0[i,j]*postseismic(co0,tcar).g(dates) + \
-        cos1[i,j]*coseismic(co1).g(dates) + ref[i,j]
+maps_clean = np.zeros((nlign,ncol,N))
+demcor = np.zeros((nlign,ncol,N))
 
-# remove model
-# maps_clean = np.zeros((nlign,ncol,N))
-# for l in xrange((N)):
-#     cst = np.nanmean(maps[:,:,l] - model[:,:,l])
-#     maps_clean[:,:,l] = maps[:,:,l] - model[:,:,l] 
-maps_clean = maps - model
+# plt.imshow(refmap)
+# plt.show()
 
-save
+# Ini Model to DEMCOR for the whole map
+# Remove ref Frame for the whole map
+for l in xrange((N)):
+    demcor[:,:,l] = demmap*base[l]
+    model[:,:,l] =  demcor[:,:,l] + refmap
+
+
+for i in xrange(ibeg,iend): # lines
+    for j in xrange(jbeg,jend): # cols
+
+        lin, a, b = 0, 0, 0
+        steps, trans, sse = np.zeros(len(cotimes)), np.zeros(len(postimes)),np.zeros(len(sse_times))
+        if slopef is not None:
+            lin = slopemap[i,j]
+        if cosf is not None:
+            a,b = cosmap[i,j], sinmap[i,j]
+        for l in xrange(len(cotimes)):
+            steps[l] = coseismaps[l,i,j]
+            trans[l] = postmaps[l,i,j]
+        for l in xrange(len(sse_times)):
+            sse[l] = ssemaps[l,i,j]
+        ###################################################
+        model[i,j,:] = model[i,j,:] + linear(tdec,lin) + seasonal(tdec, a, b)
+        for l in xrange((M)):
+            model[i,j,:] = model[i,j,:] + coseismic(tdec, cotimes[l], steps[l]) + \
+            postseismic(tdec,cotimes[l],postimes[l],trans[l])
+        for l in xrange((L)):
+            model[i,j,:] = model[i,j,:] + sse[l]*slowslip(tdec,sse_times[l],sse_car[l])
+        ####################################################
+        maps_clean[i,j,:] = maps[i,j,:] - model[i,j,:]
+
+        ###############################################################
+        # plot TS
+        if (i in ipix and j==jpix[ipix.index(i)]):
+            ax = fig.add_subplot(Npix,1,k+1)
+            print 'plot TS: {0}-{1}'.format(i,j)
+            x = [date2num(datetime.datetime.strptime('{}'.format(d),'%Y%m%d')) for d in idates]
+
+            dmax = str(datemax) + '0101'
+            dmin = str(datemin) + '0101'
+            xmin = datetime.datetime.strptime('{}'.format(dmin),'%Y%m%d') 
+            xmax = datetime.datetime.strptime('{}'.format(dmax),'%Y%m%d')
+            xlim=date2num(np.array([xmin,xmax]))
+            ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y/%m/%d"))
+            # t = np.array([xmin + datetime.timedelta(days=d) for d in range(0, 2920)]) 
+            # print coseismic(tdec, cotimes[0], steps[0])*rad2mm
+            # print postseismic(tdec,cotimes[0],postimes[0],trans[0])*rad2mm
+            # print
+            ax.plot(x,(maps[i,j,:]-demcor[i,j])*rad2mm,'o',label='line: {}, col: {}'.format(i,j))
+            if np.std(model[i,j,:]) > 0:
+                plt.plot(x,(model[i,j,:]-demcor[i,j])*rad2mm,'-r')
+            plt.legend(loc='best', fontsize='x-small')
+            ax.set_xlim(xlim)
+            if arguments["--bounds"] is not  None:
+                ax.set_ylim(ylim)
+            k = k+1
+
+
+# save fig
+fig.autofmt_xdate()
+plt.xlabel('Time (Year/month/day)')
+plt.ylabel('Displacements')
+
+# save
 fid = open('depl_cumule_nomodel', 'wb')
 maps_clean.flatten().astype('float32').tofile(fid)
 fid.close()
 
-# plot TS
-rad2mm = -4.4563
-vmax,vmin=30,-30
+###############################################################
+# plot maps
+fig = plt.figure(2,figsize=(10,9))
+fig.subplots_adjust(wspace=0.001)
+
 maps_clean = maps_clean*rad2mm
 maps = maps*rad2mm
 model = model*rad2mm
-
-ipix = 2380,2411,2250,2200
-jpix = 571,640,510,400
-Npix = len(ipix)
-fig = plt.figure(0,figsize=(10,9))
-fig.subplots_adjust(wspace=0.001)
-for jj in xrange((Npix)):
-    i, j = ipix[jj], jpix[jj]
-
-    disp_clean = maps_clean[i,j,:]
-    disp = maps[i,j,:]
-    mdisp = model[i,j,:]
-
-    x = [date2num(datetime.datetime.strptime('{}'.format(d),'%Y%m%d')) for d in idates]
-   
-    dmax = str(datemax) + '0101'
-    dmin = str(datemin) + '0101'
-    xmin = datetime.datetime.strptime('{}'.format(dmin),'%Y%m%d')
-    xmax = datetime.datetime.strptime('{}'.format(dmax),'%Y%m%d')
-    xlim=date2num(np.array([xmin,xmax]))
-    xlim=date2num(np.array([xmin,xmax]))
-    ax = fig.add_subplot(Npix,1,jj+1)
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y/%m/%d"))
-
-    ax.plot(x,disp,'o',label='lign:{}, column:{}'.format(i,j))
-    ax.plot(x,disp_clean,'x')
-    
-    t = np.array([xmin + datetime.timedelta(days=d) for d in range(0, 2920)])
-    tdec = np.array([float(date.strftime('%Y')) + float(date.strftime('%j'))/365.1 for date in t])
-    m = ( cos0[i,j]*coseismic(co0).g(tdec) +post0[i,j]*postseismic(co0,tcar).g(tdec) + \
-        cos1[i,j]*coseismic(co1).g(tdec) + ref[i,j] )*rad2mm
-
-    # m = (np.ones(len(t))*ref[i,j])*rad2mm
-
-    ax.plot(t,m,'-r')
-
-    ax.set_xlim(xlim)
-    fig.autofmt_xdate()
-    ax.set_xlabel('Time (Year/month/day)')
-    ax.set_ylabel('Displacements (mm)')
-
-# plt.show()
-# sys.exit()
-
-# plot diplacements maps
-fig = plt.figure(1,figsize=(10,9))
-fig.subplots_adjust(wspace=0.001)
+if arguments["--bounds"] is not  None:
+      vmax,vmin = np.nanmax(ylim), np.nanmin(ylim)
+else:
+    vmax = np.nanpercentile(maps_clean[:,:,-1],90)
+    vmin = np.nanpercentile(maps_clean[:,:,-1],10)
 
 for l in xrange((N)):
     d = as_strided(maps_clean[ibeg:iend,jbeg:jend,l])
@@ -166,8 +374,9 @@ plt.suptitle('Corrected TS')
 fig.colorbar(cax, orientation='vertical',aspect=10)
 fig.savefig('maps_clean_models.eps', format='EPS',dpi=150)
 
+
 # plot diplacements maps
-fig = plt.figure(2,figsize=(10,9))
+fig = plt.figure(3,figsize=(10,9))
 fig.subplots_adjust(wspace=0.001)
 
 for l in xrange((N)):
@@ -185,9 +394,8 @@ setp(ax.get_yticklabels(), visible=False)
 fig.tight_layout()
 plt.suptitle('TS')
 fig.colorbar(cax, orientation='vertical',aspect=10)
-
-
 plt.show()
+
 
 
 
