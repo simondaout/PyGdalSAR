257e5182c94519b34fd8a6cf6d77ef4222f8acc6
simondaout <simondaout@gmail.com>
2019-03-05 15:54:43 +0000

    add vmax vmin

--- plots/plot_raster.py
+++ plots/plot_raster.py
@@ -12,19 +12,29 @@
 """\
 plot\_raster.py
 -------------
-Display and Cut image file 
+Display and Cut image file (.unw/.int/.r4/.tiff)
 
-Usage: plot\_raster.py --infile=<path> [--cpt=<values>] [--wrap=<values>] [<ibeg>] [<iend>] [<jbeg>] [<jend>] [--format=<value>]
+Usage: plot\_raster.py --infile=<path> [--cpt=<values>] [<ibeg>] [<iend>] [<jbeg>] [<jend>] \
+[--format=<value>] [--lectfile=<value>] [--rad2mm=<value>] [--title=<value>] [--wrap=<values>] 
+       plot\_raster.py --infile=<path> [--cpt=<values>] [<ibeg>] [<iend>] [<jbeg>] [<jend>] \
+[--format=<value>] [--lectfile=<value>] [--rad2mm=<value>] [--title=<value>] [--wrap=<values>] [--vmin=<value>] [--vmax=<value>]
+
+
 Options:
--h --help           Show this screen.
---infile PATH       File to be cut
---ibeg VALUE        Ligne numbers bounded the cutting zone [default: 0]
---iend VALUE        Ligne numbers bounded the cutting zone [default: nlign]
---jbeg VALUE        Column numbers bounded the cutting zone [default: 0]
---jend VALUE        Column numbers bounded the cutting zone [default: ncol]
---cpt               Indicate colorscale for phase
---wrap  VALUE       Wrapped phase between value for unwrapped files [default: no]
---format VALUE        Format input files: ROI_PAC, GAMMA, GTIFF [default: ROI_PAC]
+-h --help             Show this screen.
+--infile=<file>       Raster to be displayed 
+--ibeg=<value>        Ligne numbers bounded the cutting zone [default: 0]
+--iend=<value>        Ligne numbers bounded the cutting zone [default: nlign]
+--jbeg=<value>        Column numbers bounded the cutting zone [default: 0]
+--jend=<value>        Column numbers bounded the cutting zone [default: ncol]
+--format=<value>      Format input files: ROI_PAC, GAMMA, GTIFF [default: ROI_PAC]
+--cpt==<value>        Indicate colorscale for phase
+--wrap=<value>        Wrapped phase between value for unwrapped files 
+--lectfile=<file>      Path of the lect.in file for r4 format
+--rad2mm=<value>      Convert data [default: 1]
+--tile=<value>        Title plot 
+--vmax              Max colorscale [default: 98th percentile]
+--vmin              Min colorscale [default: 2th percentile]
 """
 
 from __future__ import print_function
@@ -63,20 +73,42 @@ if arguments["--format"] ==  None:
 else:
     sformat = arguments["--format"]
 
-ds_extension = os.path.splitext(sys.argv[1])[1]
+if arguments["--lectfile"] ==  None:
+    lecfile = "lect.in"
+else:
+    lecfile = arguments["--lectfile"]
+
+if arguments["--rad2mm"] ==  None:
+        rad2mm = 1
+else:
+        rad2mm = np.float(arguments["--rad2mm"])
+
+ds_extension = os.path.splitext(infile)[1]
+print(ds_extension)
 
 if sformat == "ROI_PAC": 
     if (ds_extension == ".unw" or ds_extension ==".hgt"):
         phase_band = ds.GetRasterBand(2)
         amp_band = ds.GetRasterBand(1)
+        # Attributes
+        print("> Driver:   ", ds.GetDriver().ShortName)
+        print("> Size:     ", ds.RasterXSize,'x',ds.RasterYSize,'x',ds.RasterCount)
+        print("> Datatype: ", gdal.GetDataTypeName(phase_band.DataType))
+        nlines, ncols = ds.RasterYSize, ds.RasterXSize
+    elif (ds_extension == ".r4" or ds_extension == ""):
+        fid = open(infile, 'r')
+        ncols, nlines = map(int, open(lecfile).readline().split(None, 2)[0:2])
+        phi = np.fromfile(fid,dtype=np.float32)[:nlines*ncols].reshape((nlines,ncols))
+        print("> Driver:   REAL4  band file")
+        print("> Size:     ", ncols,'x',nlines,'x')
+        print("> Datatype: FLOAT32")
     else:
         phase_band = ds.GetRasterBand(1)
-
-    # Attributes
-    print("> Driver:   ", ds.GetDriver().ShortName)
-    print("> Size:     ", ds.RasterXSize,'x',ds.RasterYSize,'x',ds.RasterCount)
-    print("> Datatype: ", gdal.GetDataTypeName(phase_band.DataType))
-    nlines, ncols = ds.RasterYSize, ds.RasterXSize
+        # Attributes
+        print("> Driver:   ", ds.GetDriver().ShortName)
+        print("> Size:     ", ds.RasterXSize,'x',ds.RasterYSize,'x',ds.RasterCount)
+        print("> Datatype: ", gdal.GetDataTypeName(phase_band.DataType))
+        nlines, ncols = ds.RasterYSize, ds.RasterXSize
 
 elif sformat == "GTIFF":
     phase_band = ds.GetRasterBand(1)
@@ -108,7 +140,6 @@ if arguments["<jend>"] ==  None:
 else:
     jend = int(arguments["<jend>"])
 
-
 if sformat == "ROI_PAC":
     if (ds_extension == ".unw" or ds_extension ==".hgt"):
         phi = phase_band.ReadAsArray(0, 0,
@@ -119,59 +150,64 @@ if sformat == "ROI_PAC":
                                ds.RasterXSize, ds.RasterYSize)
         
         # cut image
-        cutphi = as_strided(phi[ibeg:iend,jbeg:jend])
+        cutphi = as_strided(phi[ibeg:iend,jbeg:jend])*rad2mm
         cutamp = as_strided(amp[ibeg:iend,jbeg:jend])
 
         # Unwrapped inteferogram
         hax = ax.imshow(cutamp, cm.Greys,vmax=np.percentile(cutamp, 95))
 
-        if arguments["--wrap"] is not None:
-            cutphi =  np.mod(cutphi+float(arguments["--wrap"]),2*float(arguments["--wrap"]))-float(arguments["--wrap"])
-            vmax=float(arguments["--wrap"])
-            vmin = -vmax 
-        else:
-            vmax=np.nanmean(cutphi)+2*np.nanstd(cutphi)
-            vmin=np.nanmean(cutphi)-2*np.nanstd(cutphi)
+    elif (ds_extension == ".r4" or ds_extension == ""):
 
-        cax = ax.imshow(cutphi, cmap, interpolation=None,vmax=vmax,vmin=vmin,alpha=0.6)
+        cutphi = as_strided(phi[ibeg:iend,jbeg:jend])*rad2mm 
 
     else:
         phi = phase_band.ReadAsArray(0, 0,
                                ds.RasterXSize, ds.RasterYSize,
                                ds.RasterXSize, ds.RasterYSize)
         
-        cutphi = as_strided(phi[ibeg:iend,jbeg:jend]) 
-      
+        cutphi = as_strided(phi[ibeg:iend,jbeg:jend])*rad2mm 
+        
         if ds_extension == ".slc":
             # SLC, display amplitude
             cutphi = np.absolute(cutphi)
-            cax = ax.imshow(cutphi, cm.Greys_r, vmax=np.percentile(cutphi, 95))
         elif ds_extension in [".int", ".flat"]:
-
             # Wrapped interferogram, display computed phase
             cutamp = np.absolute(cutphi)
             cutphi = np.angle(cutphi)
             hax = ax.imshow(cutamp, cm.Greys_r)
-            cax = ax.imshow(cutphi, cmap,alpha=0.6)
-            # hax = ax.imshow(cutamp, cm.Greys,vmax=1,vmin=0.)
-        elif ds_extension ==  ".trans":
-            # Geocoding lookup table
-            cax = ax.imshow(cutphi)
 
 elif sformat == "GTIFF":
     phi = phase_band.ReadAsArray(0, 0,
            ds.RasterXSize, ds.RasterYSize,
            ds.RasterXSize, ds.RasterYSize)
+    cutphi = as_strided(phi[ibeg:iend,jbeg:jend])*rad2mm
 
-    cutphi = as_strided(phi[ibeg:iend,jbeg:jend]) 
-    cax = ax.imshow(cutphi, cmap,alpha=0.6, interpolation=None)
 elif sformat == "GAMMA":
-    cutphi = as_strided(phi[ibeg:iend,jbeg:jend]) 
-    cax = ax.imshow(cutphi, cmap,alpha=0.6, interpolation=None)
+    cutphi = as_strided(phi[ibeg:iend,jbeg:jend])*rad2mm
+
+
+if arguments["--wrap"] is not None:
+    cutphi =  np.mod(cutphi+float(arguments["--wrap"]),2*float(arguments["--wrap"]))-float(arguments["--wrap"])
+    vmax=float(arguments["--wrap"])
+    vmin = -vmax
+elif (arguments["--vmax"] is not None) or (arguments["--vmin"] is not None):
+    if arguments["--vmax"] is not  None:
+        vmax = np.float(arguments["--vmax"])
+    if arguments["--vmin"] is not  None:
+        vmin = np.float(arguments["--vmin"])
+else:
+    vmax = np.nanpercentile(cutphi,95)
+    vmin = np.nanpercentile(cutphi,5)
+
+cax = ax.imshow(cutphi, cmap, interpolation=None,vmax=vmax,vmin=vmin,alpha=0.6)
 
 divider = make_axes_locatable(ax)
 c = divider.append_axes("right", size="5%", pad=0.05)
 plt.colorbar(cax, cax=c)
+if arguments["--title"] ==  None:
+    fig.canvas.set_window_title(infile)
+else:
+    fig.canvas.set_window_title(arguments["--title"])
 
 try:
     del ds
@@ -179,7 +215,6 @@ except:
     pass
 
 # Display the data
-fig.canvas.set_window_title(sys.argv[1])
 ##ax.set_rasterized(True)
 fig.savefig('{}.eps'.format(infile), format='EPS',dpi=150)
 plt.show()
