f85434021f5f914355fe55ec90d7223ab3f7e5ca
simondaout <simondaout@gmail.com>
2019-03-26 14:51:44 +0000

    replace crappy f95 prog to python

--- utils/bridge.py
+++ utils/bridge.py
@@ -9,6 +9,7 @@
 bridge.py
 ========================
 Create bridge.in file given output form mdx wirtten in a tex file
+To create the text file: right clic on the two points to bridge on mdx windows and copy past terminal in text file
 
 Usage:
   bridge.py  <file>  
@@ -18,20 +19,31 @@ Options:
 """
 
 import docopt
-import numpy as np
-from collections import namedtuple
 import re
 
 arguments = docopt.docopt(__doc__)
 infile = arguments["<file>"]
 
 
-LINE_EXPR = r"(?P<option>.*?)\s*(?P<vi>:)\s*(?P<value>.*)$"
-_re = re.compile(LINE_EXPR.format(delim=":"), re.VERBOSE)
+# there is probably a better way to do that re search... 
+_re = re.compile(r"(.*) COL:(.*?) ROW:(.*?) (\d+) (.*)", re.IGNORECASE)
 
+file = open('bridge.in', "w")
 lines = []
 with open(infile) as in_file:
-	for line in in_file:
-		lines.append(line.strip())
-print(lines)
+    for i, line in enumerate(in_file):
+        if line.strip().startswith("#"):
+            continue
+        line = line.strip()
+        if (not line): 
+            continue
+
+        m = _re.match(line)
+        if m:
+            col, row =  m.group(2).strip(), m.group(4).strip()
+            print(i, col, row)
+            if i % 2:
+                file.write("%s %s 0\n" % (str(col), str(row)))
+            else:
+                file.write("%s %s " % (str(col), str(row)))
 
