52d66aba8cd0cc5c076b82f44cddfc261f21ca47
simondaout <simondaout@gmail.com>
2019-07-24 20:51:08 +0100

    read incidence file

--- utils/dem2slope.py
+++ utils/dem2slope.py
@@ -15,7 +15,10 @@ dem2slope.py
 -------------
 Convert dem to slope file in the LOS geeometry knowing los heading and lattitude mean
 
-Usage: dem2slope.py --infile=<path> --outfile=<path> --los=<value> --heading=<value> --lat=<value>
+Usage: 
+    dem2slope.py --infile=<path> --outfile=<path> --los=<value> --heading=<value> --lat=<value>
+    dem2slope.py --infile=<path> --outfile=<path> --incidence=<path> --lat=<value>
+
 dem2slope.py -h | --help
 
 Options:
@@ -24,7 +27,8 @@ Options:
 --outfile PATH      Output file 
 --los VALUE         Mean Los angle
 --heading VALUE.    Mean Heading angle
---lat VALUE.    	Average latitude
+--lat VALUE.        Average latitude in deg.
+--incidence=<file>  Path to incidence file .unw 
 """
 
 import gdal, os
@@ -33,44 +37,55 @@ import docopt
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
+np.warnings.filterwarnings('ignore')
 
 # read arguments
 arguments = docopt.docopt(__doc__)
 infile = arguments["--infile"]
 outfile = arguments["--outfile"]
-look = float(arguments["--los"])*np.pi/180
-heading = float(arguments["--heading"])*np.pi/180
-lat = float(arguments["--lat"])
+lat = np.deg2rad(float(arguments["--lat"]))
 
 # read input
 ds_extension = os.path.splitext(infile)[1]
 if (ds_extension == ".r4" or ds_extension == ""):
-	fid = open(infile, 'r')
-	ncols, nlines = map(int, open('lect.in').readline().split(None, 2)[0:2])
-	topo = np.fromfile(fid,dtype=np.float32)[:nlines*ncols].reshape((nlines,ncols))
-	fid.close()
+    fid = open(infile, 'r')
+    ncols, nlines = map(int, open('lect.in').readline().split(None, 2)[0:2])
+    topo = np.fromfile(fid,dtype=np.float32)[:nlines*ncols].reshape((nlines,ncols))
+    fid.close()
 else:
-	ds = gdal.Open(infile, gdal.GA_ReadOnly)
-	ds_band = ds.GetRasterBand(1)
-	topo = ds_band.ReadAsArray()
+    ds = gdal.Open(infile, gdal.GA_ReadOnly)
+    ds_band = ds.GetRasterBand(1)
+    topo = ds_band.ReadAsArray()
+    ncols, nlines = ds.RasterYSize, ds.RasterXSize
 
+look, heading = np.zeros((nlines,ncols)),np.zeros((nlines,ncols))
+if arguments["--incidence"] == None:
+    look.fill(np.deg2rad(float(arguments["--los"])))
+    heading.fill(np.deg2rad(float(arguments["--heading"])))
+else:
+    ds = gdal.Open(arguments["--incidence"], gdal.GA_ReadOnly)
+    ds_band1 = ds.GetRasterBand(1)
+    ds_band2 = ds.GetRasterBand(2)
+    look[:,:] = np.deg2rad(ds_band1.ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize))[:nlines,:ncols]
+    # in roipac incidence file, band 2 is the angle between north and horizontal LOS
+    heading[:,:] = np.deg2rad(90 - ds_band2.ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize))[:nlines,:ncols]
 
-topo = ds_band.ReadAsArray()
-toposmooth = scipy.ndimage.filters.gaussian_filter(topo,.1)
-Py, Px = np.gradient(toposmooth,90,90*np.cos(lat*np.pi/180))
+toposmooth = scipy.ndimage.filters.gaussian_filter(topo,5.)
+Py, Px = np.gradient(toposmooth,90,90*np.cos(lat))
 slope = np.sqrt(Px**2+Py**2)
 slopelos = (np.cos(heading)*Px+np.sin(heading)*Py)/np.sin(look)
 
 if (ds_extension == ".r4" or ds_extension == ""):
-	fid1 = open(outfile,'wb')
-	slopelos.flatten().astype('float32').tofile(fid1)
+    fid1 = open(outfile,'wb')
+    slopelos.flatten().astype('float32').tofile(fid1)
+    print(np.shape(slopelos))
 else:
-	# create output file
-	drv = gdal.GetDriverByName('GTiff')
-	ds2 = drv.CreateCopy(outfile,ds)
-	ds2_band = ds2.GetRasterBand(1)
-	ds2_band.WriteArray(slopelos)
-	del ds2
+    # create output file
+    drv = gdal.GetDriverByName('GTiff')
+    ds2 = drv.CreateCopy(outfile,ds)
+    ds2_band = ds2.GetRasterBand(1)
+    ds2_band.WriteArray(slopelos)
+    del ds2
 
 # ibeg,iend=600,900
 # jbeg,jend=900,1200
