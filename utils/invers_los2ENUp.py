5126ea4ad80f6786f7f8500be9768d1895dd80fe
simondaout <simondaout@gmail.com>
2019-07-29 17:05:50 +0100

    wip

---
+++ utils/invers_los2ENUp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+############################################
+#
+# PyGdalSAR: An InSAR post-processing package 
+# written in Python-Gdal
+#
+############################################
+# Author        : Simon DAOUT (Oxford)
+############################################
+
+
+infile1 = "T172_inter_LOSVelocity_nan_s90.xy-los_flat.tiff"
+look1 = "T172_look.tiff"
+heading1 = "T172_head.tiff"
+
