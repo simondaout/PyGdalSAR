12d1b9307454e1e53bca0c86e76d48f110b40efe
simondaout <simondaout@gmail.com>
2019-09-05 10:18:18 +0100

    improve plots

--- atmocorr/extract_t2m_from_datanc.py
+++ atmocorr/extract_t2m_from_datanc.py
@@ -33,7 +33,7 @@ for b in range(1, ds.RasterCount+1):
     # convert date to dec
     dec = float(date.strftime('%j'))/365.1
     year = float(date.strftime('%Y'))
-    # print date,dec,year
+    print date,dec,year
     mode.append(dec)
     time.append(year+dec)
     
@@ -54,28 +54,44 @@ time,dates,mode = np.array(time), np.array(dates),np.array(mode)
 # Initialisation
 nfigure = 0
 phase,amplitude = np.zeros((ds.RasterYSize,ds.RasterXSize)),np.zeros((ds.RasterYSize,ds.RasterXSize))
-datemin, datemax= np.int(np.min(time)), np.int(np.min(time))+1
+datemin, datemax= np.int(np.min(time)), np.int(np.max(time))+1
+# print(datemin,datemax)
+# sys.exit()
 doplot='yes'
 
 def seasonal(t,v,a,b,c):
-    return v*(t-datemin) + a*np.cos(2*np.pi*(time-datemin)) + b*np.sin(2*np.pi*(time-datemin)) + c
+    return v*t + a*np.cos(2*np.pi*t) + b*np.sin(2*np.pi*t) + c
 
-def invers(temp,time,datemin):
+def invers(temp,time):
     # fit
     G = np.zeros((len(temp),4))
-    G[:,0] = time-datemin
-    G[:,1] = np.cos(2*np.pi*(time-datemin))
-    G[:,2] = np.sin(2*np.pi*(time-datemin))
+    G[:,0] = time
+    G[:,1] = np.cos(2*np.pi*(time))
+    G[:,2] = np.sin(2*np.pi*(time))
     G[:,3] = 1
 
     pars = np.dot(np.dot(np.linalg.inv(np.dot(G.T,G)),G.T),temp)
     v,a,b,c = pars[0],pars[1],pars[2],pars[3]
-    fit = v*(time-datemin) + a*np.cos(2*np.pi*(time-datemin)) + b*np.sin(2*np.pi*(time-datemin)) + c
+    # fit = v*(time-datemin) + a*np.cos(2*np.pi*(time-datemin)) + b*np.sin(2*np.pi*(time-datemin)) + c
     return v, a, b, c 
 
-def plot(dates,time,mode,temp,fit,amp,dphi,nfigure,datemin,datemax):
+def plot(dates,time,mode,temp):
+
+    global nfigure, datemin, datemax
+
+    v, a, b, c = invers(temp, time-datemin)
+    amp,dphi=np.sqrt(a**2+b**2),np.arctan2(b,a)
+    if dphi < 0:
+        dphi = dphi + 2*np.pi
+    phase[i,j], amplitude[i,j] = dphi,amp
+    print('Mean T: {:0.3f} °'.format(np.mean(t)))
+    print('Vitesse: {:0.3f} °/yr'.format(v))
+    print('Amplitude: {:0.3f} °'.format(np.sqrt(a**2+b**2)))
+    print('Phase Shift: {:0.3f} rad'.format(np.arctan2(b,a)))
+    fit = seasonal(time-datemin,v,a,b,c)
+
     # plot data
-    fig = plt.figure(nfigure,figsize=(7,6))
+    fig = plt.figure(nfigure,figsize=(12,5))
     ax = fig.add_subplot(1,1,1)
     # convert idates to num
     x = date2num(dates)
@@ -83,10 +99,9 @@ def plot(dates,time,mode,temp,fit,amp,dphi,nfigure,datemin,datemax):
     xlim=date2num(np.array([xmin,xmax]))
     # format the ticks
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y/%m/%d"))
-    ax.plot(x,temp, '-bo',ms=.5,lw=.1,color='blue',label='ERAI point {}, {}'.format(lat,lon))
-    ax.plot(x,fit,'-r',lw=2.0,label='v: {:0.2f} deg/yrs, dphi:{:0.2f} rad, Amp:{:0.2f} deg'.format(v,dphi,amp))
-    #ax.plot(xlim,[273.15,273.15],'-b')
-    # axes up to make room for them
+    ax.plot(x,temp, '-bo',ms=.5,lw=.1,color='blue',label='ERAI point {}, {}'.format(lat,lon),rasterized=True)
+    ax.plot(x,fit,'-r',lw=2.0,label='MAAT: {0:.2f}, v: {1:.2f} deg/yrs, dphi:{2:.2f} rad, Amp:{3:.2f} deg'.format(np.mean(temp),v,dphi,amp))
+    ax.plot(xlim,[0,0],'--',c='black')
     fig.autofmt_xdate()
     ax.set_xlim(xlim)
     ax.set_ylim([-30,30])
@@ -95,40 +110,42 @@ def plot(dates,time,mode,temp,fit,amp,dphi,nfigure,datemin,datemax):
     plt.legend(loc='best')
     fig.savefig('temperatures_{}_{}.pdf'.format(lat,lon), format='PDF')
     nfigure=+1 
+
     # plot mod
     fig2 = plt.figure(nfigure,figsize=(7,5))
     ax2 = fig2.add_subplot(1,1,1)
     ax2.set_xlim([0,1])
-    ax2.plot(mode,temp, '.k',ms=0.5,color='blue',label='ERAI point {}, {}'.format(lat,lon))
-    #ax2.plot([0,1],[273.15,273.15],'-b')
+    ax2.plot(mode,temp, '-bo',ms=.5,lw=.1,color='blue',label='ERAI point {}, {}'.format(lat,lon),rasterized=True)
+    x = np.arange(0,1,0.01)
+    fit = seasonal(x,v,a,b,c)
+    ax2.plot(x,fit,'-r',lw=2.0,label='MAAT: {0:.2f}'.format(np.mean(temp)))
+    ax2.plot([0,1],[0,0],'--',c='black')
+
+    idx = np.argwhere(np.diff(np.sign(fit))).flatten()
+    for k in range(len(idx)):
+        ax2.text(x[idx[k]],-25,'{:0.3f}'.format(x[idx[k]]))
+        ax2.text(x[idx[k]],-28,(datetime.datetime(np.int(datemin), 1, 1) + datetime.timedelta(x[idx[k]]*365.1 - 1)).strftime('%Y/%m/%d'))
+        ax2.plot([x[idx[k]],x[idx[k]]],[-30,30],'--',c='black')
+
     ax2.set_xticks(np.arange(0, 1, 1./12))
     ax2.xaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
     plt.xlabel('Time')
     plt.ylabel('Temperature (deg)')
     plt.legend(loc='best')
+    ax2.set_ylim([-30,30])
     fig2.savefig('Modtemperatures_{}_{}.pdf'.format(lat,lon), format='PDF')
 
 for i in xrange(ds.RasterYSize):
     lat = ds.GetGeoTransform()[3]-0.125 + ds.GetGeoTransform()[5]*i
     for j in xrange(ds.RasterXSize):
         lon = ds.GetGeoTransform()[0]+0.125 + ds.GetGeoTransform()[1]*j
-        print 'lon: {}, lat: {}'.format(lon,lat)
+        print('lon: {}, lat: {}'.format(lon,lat))
         t = temp[i,j,:]
-        v, a, b, c = invers(t,time,datemin)
-        amp,dphi=np.sqrt(a**2+b**2),np.arctan2(b,a)
-        if dphi < 0:
-            dphi = dphi + 2*np.pi
-        phase[i,j], amplitude[i,j] = dphi,amp
-        print('Mean T: {:0.3f} °'.format(np.mean(t)))
-        print('Vitesse: {:0.3f} °/yr'.format(v))
-        print('Amplitude: {:0.3f} °'.format(np.sqrt(a**2+b**2)))
-        print('Phase Shift: {:0.3f} rad'.format(np.arctan2(b,a)))
-        print
         if doplot=='yes':
-            fit = seasonal(time,v,a,b,c)
-            plot(dates,time,mode,t,fit,amp,dphi,nfigure,datemin,datemax)
+            plot(dates,time,mode,t)
             nfigure+=2
-            # plt.show()
+            plt.show()
+            # plt.close()
             # sys.exit()
 
         # save data
