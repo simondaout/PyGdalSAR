bdb3a653019826329c42a15b5dd5d425b9e7b340
simondaout <simondaout@gmail.com>
2020-04-13 13:34:02 +0100

    add vmin/vmax rad2mm

--- utils/add_r4.py
+++ utils/add_r4.py
@@ -14,7 +14,7 @@ add_r4.py
 -------------
 Add, substract, compute the amplitude (infile1**2,infile2**2) ar the phase (arctan(infile1/infile2)) from two real4 files.
 
-Usage: add_r4.py --infile1=<path> --infile2=<path> [--outfile=<path>] [--lectfile=<path>] --sign=<+/-/amp/dphi> 
+Usage: add_r4.py --infile1=<path> --infile2=<path> [--outfile=<path>] [--lectfile=<path>] --sign=<+/-/amp/dphi> [--rad2mm=<value>] [--vmin=<value>] [--vmax=<value>] 
 add_r4.py -h | --help
 
 Options:
@@ -24,6 +24,9 @@ Options:
 --outfile PATH
 --lectfile PATH     Path of the lect.in file [default: lect.in]
 --sign VALUE        amp=sqrt(infile1**2,infile2**2), dphi=arctan(infile1/infile2)
+--rad2mm=<value>      Convert data [default: 1]
+--vmax                Max colorscale [default: 98th percentile]
+--vmin                Min colorscale [default: 2th percentile]
 """
 
 import numpy as np
@@ -48,10 +51,13 @@ else:
 # read lect.in
 ncol, nlign = map(int, open(lecfile).readline().split(None, 2)[0:2])
 
-if arguments["--outfile"] ==  None:
-    outfile  = 'diff.r4'
-else:
+if arguments["--outfile"] is not  None:
     outfile = arguments["--outfile"]
+
+if arguments["--rad2mm"] ==  None:
+        rad2mm = 1
+else:
+        rad2mm = np.float(arguments["--rad2mm"])
 
 # read r4
 in1 = np.fromfile(infile1,dtype=np.float32) 
@@ -76,16 +82,22 @@ if sign=='amp':
 if sign=='dphi':
     out = np.arctan2(in11,in22)
 
-# save outfile in the convention ROIPACK
-out2 = out
-out2.flatten().astype('float32').tofile(outfile)
+# save outfile in the convention ROIPAC
+if arguments["--outfile"] is not  None:
+  out.flatten().astype('float32').tofile(outfile)
 
-# remove NaN to compute colorbar
-index = np.nonzero(np.isnan(out))
-#out[index] = 0
+# convert
+in11, in22, out = in11*rad2mm, in22*rad2mm, out*rad2mm
 
 # plot maps
-vmax = np.max( [np.nanpercentile(in11,95),np.abs(np.nanpercentile(in11,5))] )
+if arguments["--vmax"] is not  None:
+    vmax = np.float(arguments["--vmax"])
+else:
+    vmax = np.max( [np.nanpercentile(in11,95),np.abs(np.nanpercentile(in11,5))] )
+if arguments["--vmin"] is not  None:
+    vmin = np.float(arguments["--vmin"])
+else:
+    vmin = -vmax
 
 try:
         from matplotlib.colors import LinearSegmentedColormap
@@ -98,7 +110,7 @@ except:
 fig = plt.figure(10, figsize=(10,8))
 fig.subplots_adjust(hspace=0.5)
 ax1 = fig.add_subplot(1,3,1)
-cax = ax1.imshow(in11, cmap = cmap, vmax=vmax, vmin=-vmax, extent=None)
+cax = ax1.imshow(in11, cmap = cmap, vmax=vmax, vmin=vmin, extent=None)
 divider = make_axes_locatable(ax1)
 c = divider.append_axes("right", size="5%", pad=0.05)
 plt.colorbar(cax, cax=c)
@@ -106,18 +118,17 @@ setp( ax1.get_xticklabels(), visible=False)
 ax1.set_title(infile1)
 
 ax2 = fig.add_subplot(1,3,2)
-cax = ax2.imshow(in22, cmap = cmap, vmax=vmax, vmin=-vmax, extent=None)
+cax = ax2.imshow(in22, cmap = cmap, vmax=vmax, vmin=vmin, extent=None)
 setp( ax2.get_xticklabels(), visible=False)
 ax2.set_title(infile2)
 divider = make_axes_locatable(ax2)
 c = divider.append_axes("right", size="5%", pad=0.05)
 plt.colorbar(cax, cax=c)
-#vmax = np.max( [np.nanpercentile(out,98),np.abs(np.nanpercentile(out,2))] )
 
-vmax = np.max( [np.nanpercentile(out,95),np.abs(np.nanpercentile(out,5))] )
+#vmax = np.max( [np.nanpercentile(out,95),np.abs(np.nanpercentile(out,5))] )
 
 ax3 = fig.add_subplot(1,3,3)
-cax = ax3.imshow(out, cmap = cmap, vmax=vmax, vmin=-vmax, extent=None)
+cax = ax3.imshow(out, cmap = cmap, vmax=vmax, vmin=vmin, extent=None)
 setp( ax3.get_xticklabels(), visible=False)
 ax3.set_title(outfile)
 divider = make_axes_locatable(ax3)
@@ -125,7 +136,7 @@ c = divider.append_axes("right", size="5%", pad=0.05)
 plt.colorbar(cax, cax=c)
 fig.tight_layout()
 
-fig.savefig('{}.pdf'.format('add_raster'), format='PDF',dpi=150)
-
+if arguments["--outfile"] is not  None:
+  fig.savefig('{}.pdf'.format('outfile'), format='PDF',dpi=150)
 
 plt.show()
