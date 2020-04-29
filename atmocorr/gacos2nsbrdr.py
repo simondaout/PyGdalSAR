1d9b7ff30f9a44392671d7766e95631e9061fa54
simondaout <simondaout@gmail.com>
2020-04-29 14:52:46 +0100

    crop/resample with gdal, read files with gdal, add logging, ....

--- atmocorr/gacos2nsbrdr.py
+++ atmocorr/gacos2nsbrdr.py
@@ -12,10 +12,9 @@ for application to NSBAS generated IFGS.
     JD Dianala (Oxford University)
         
 Usage:
-    nsb_gacos2rdr.py --geo_rsc=<path> --rdr_rsc=<path> --lt=<path> --inc_file=<path> --sim_dem_rdr=<path> --gacos_dir=<path> --int_list=<path> [--nproc=<nb_cores>]
+    nsb_gacos2rdr.py --rdr_rsc=<path> --lt=<path> --inc_file=<path> --sim_dem_rdr=<path> --gacos_dir=<path> --int_list=<path> [--nproc=<nb_cores>] [-f]
 
 Options:
-    --geo_rsc=path              path to geoc param file
     --rdr_rsc=path              path to radar param file
     --lt=path                   path to lookup table (rdr to geoc)
     --inc_file=PATH             path to incidence angle file for S1 TOPS in radar.
@@ -23,7 +22,7 @@ Options:
     --gacos_dir=path            path to folder containing gacos ztds
     --int_list=path             path to interf pair list
     --nproc=values              number of processor (default: 1)
-
+    -f                          Force mode. Overwrite output files
 """
 
 import os, sys, logging, glob
@@ -33,10 +32,11 @@ import matplotlib.pyplot as plt
 from os import path
 from scipy import ndimage
 import scipy.constants as const
+from osgeo import osr
 
-from shutil import copyfile
+import shutil, filecmp
 from multiprocessing import Process, Pool
-import subprocess
+import subprocess,gdal
 
 import matplotlib as mpl
 from matplotlib import pyplot as plt
@@ -47,56 +47,21 @@ from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.patches as patches
 
 ### Load colormaps
-cm_locs = '/home/comethome/jdd/ScientificColourMaps5/by_platform/python/'
+cm_locs = os.environ["PYGDALSAR"] + '/contrib/python/colormaps/'
 cmap = LinearSegmentedColormap.from_list('roma', np.loadtxt(cm_locs+"roma.txt")).reversed()
 
-def geo_rsc(inname, full=False, verbose=False):
-        '''Reading a ROI-PAC style geocoded rsc file.
+def rm(file):
+    if path.exists(file):
+        os.remove(file)
 
-        Args:
-                * inname (str): Path to the RSC file.
-
-        Returns:
-                * lon (np.array) : Array of min and max lon values.
-                * lat (np.array) : Array of min and max lat values.
-                * nx  (np.int)   : Number of lon bins.
-                * ny  (np.int)   : Number of lat bins.
-
-                .. note:: 
-                        Currently set up to work with dem.rsc file from ROI-PAC.'''
-
-        if verbose:
-                logger.info("PROGRESS: READING %s RSC FILE" %inname)
-
-        rpacdict = {}
-        infile = open(inname,'r')
-        line = infile.readline()
-        while line:
-                llist = line.split()
-                if len(llist)>0 :
-                        rpacdict[llist[0]] = llist[1]
-                line = infile.readline()
-        infile.close()
-
-        nx = np.int(rpacdict['WIDTH'])
-        ny = np.int(rpacdict['FILE_LENGTH'])
-        lat=np.zeros((2,1))
-        lon=np.zeros((2,1))
-        lat[1] = np.float(rpacdict['Y_FIRST'])
-        lon[0] = np.float(rpacdict['X_FIRST'])
-        if(lon[0] < 0):
-                lon[0] = lon[0] + 360.0
-
-        dx = np.float(rpacdict['X_STEP'])
-        dy = np.float(rpacdict['Y_STEP'])
-
-        lat[0] = lat[1] + dy*ny
-        lon[1] = lon[0] + dx*nx
-
-        if full:
-                return lon,lat,nx,ny,rpacdict
+def copyrsc(src,dest):
+    if path.exists(dest):
+        if filecmp.cmp(src,dest):
+            pass
         else:
-                return lon,lat,nx,ny
+            shutil.copy(src,dest)
+    else:
+            shutil.copy(src,dest)
 
 def rdr_rsc(inname, full=False, verbose=False):
         '''Reading a ROI-PAC style radar rsc file.
@@ -133,55 +98,85 @@ def rdr_rsc(inname, full=False, verbose=False):
         else:
                 return nx,ny
 
-def gacos2geo(int_date):
-    # file path for gacos ztd file for date
+def convert(int_date):
     gacos_ztd_file = gacos_dir + '/' + str(int_date) + '.ztd'
+    rsc = gacos_dir + '/' + str(int_date) + '.ztd.rsc'
+    temp = gacos_dir + '/' + str(int_date) + '_temp.tif'
+    gacos_tif_file = gacos_dir + '/' + str(int_date) + '.tif'
+    if force:
+        rm(gacos_tif_file);rm(temp)
+
+    # read .rsc
+    nncol = np.int(open(rsc).readlines()[0].split(None, 2)[1])
+    nnlines = np.int(open(rsc).readlines()[1].split(None, 2)[1])
+    xfirst = np.float(open(rsc).readlines()[6].split(None, 2)[1])
+    yfirst = np.float(open(rsc).readlines()[7].split(None, 2)[1])
+    xstep = np.float(open(rsc).readlines()[8].split(None, 2)[1])
+    ystep = np.float(open(rsc).readlines()[9].split(None, 2)[1])
+    geotransform = (xfirst, xstep, 0, yfirst, 0, ystep)
+    logger.info('Read .rsc and set Geotransform: {}'.format(geotransform))
+    ztd_ = np.fromfile(gacos_ztd_file, dtype='float32').reshape(nnlines,nncol)
+
+     # transform gacos format to samething humanily readeable
+    driver = gdal.GetDriverByName('GTiff')
+    ds = driver.Create(temp, nncol, nnlines, 1, gdal.GDT_Float32)
+    band = ds.GetRasterBand(1)
+    band.WriteArray(ztd_)
+    ds.SetGeoTransform(geotransform)
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(4326)
+    ds.SetProjection(srs.ExportToWkt())
+    band.FlushCache()
+    del ds,band
+
+    logger.info("gdalwarp -overwrite -tr "+str(geo_dx)+" "+str(geo_dy)+" -te "+str(geo_lon[0])+" "+str(geo_lat[0])+" "+str(geo_lon[1])+" "+str(geo_lat[1])+" -r bilinear "+temp+" "+gacos_tif_file+" -of GTiff")
+    r = subprocess.call("gdalwarp -overwrite -tr "+str(geo_dx)+" "+str(geo_dy)+" -te "+str(geo_lon[0])+" "+str(geo_lat[0])+" "+str(geo_lon[1])+" "+str(geo_lat[1])+" -r bilinear "+temp+" "+gacos_tif_file+" -of GTiff", shell=True,stdout=sys.stdout, stderr=subprocess.STDOUT,env=os.environ)
+    if r != 0:
+        logger.critical(r)
+    # del temporary files
+    rm(temp)
+
+    outfile = gacos_dir+'/'+str(int_date)+'.ztd.geo'
+    if force:
+        rm(outfile); rm(outfile+'.rsc')
 
     # if radar geom gacos file does not already exist...
-    if os.path.isfile(gacos_dir+'/'+str(int_date)+'.ztd.geo') != True:
+    if os.path.isfile(outfile) != True:
         
-        # read in gacos correction parameters from ztd.rsc file header (using geo_rsc function copied from GIAnT). 
-        gacos_lon, gacos_lat, gacos_nx, gacos_ny, gacos_dict = geo_rsc(gacos_ztd_file+'.rsc', full=True, verbose=False)
-        gacos_dx = np.float(gacos_dict['X_STEP'])
-        gacos_dy = np.float(gacos_dict['Y_STEP'])
+        # read in gacos correction metadata 
+        ds = gdal.Open(gacos_tif_file, gdal.GA_ReadOnly)
+        ds_geo = ds.GetGeoTransform()
+        gacos_dx, gacos_dy = ds_geo[1], ds_geo[5]
+        gacos_nx, gacos_ny = ds.RasterXSize, ds.RasterYSize
+        gacos_lon = ds_geo[0], ds_geo[0]+ ds_geo[1]*ds.RasterXSize
+        gacos_lat = ds_geo[3]+ds_geo[5]*ds.RasterYSize, ds_geo[3] 
 
         # Make sure that GACOS coverage >= SAR geo coverage
         if ( geo_lat[0] < gacos_lat[0] or geo_lat[1] > gacos_lat[1] or \
                 geo_lon[0] < gacos_lon[0] or geo_lon[1] > gacos_lon[1] ):
-            #raise Exception('GACOS correction covers a smaller area than your SAR (re-request GACOS for larger polygon).')
-            print('GACOS correction covers a smaller area than your SAR (re-request GACOS for larger polygon).')
+            logger.warning('GACOS correction covers a smaller area than your SAR (re-request GACOS for larger polygon).')
 
         # read in gacos correction ztd and reshape
-        gacos_ztd = np.fromfile(gacos_ztd_file, dtype=np.float32).reshape(gacos_ny, gacos_nx)
-        
-        # resample factor so the GACOS data matches SAR data resolution
-        yzoom = gacos_dy / geo_dy
-        xzoom = gacos_dx / geo_dx
-        
-        # resampling
-        import warnings
-        warnings.filterwarnings('ignore', '.*output shape of zoom.*')
-        gacos_ztd_res = ndimage.zoom(gacos_ztd, [yzoom,xzoom], order=1)
-        
-        [gacos_res_ny, gacos_res_nx] = gacos_ztd_res.shape
+        gacos_ztd = ds.GetRasterBand(1).ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize)
 
-        # crop GACOS ztd to SAR area, then reshape GACOS ztd
-        cutmaxlat_i = int(np.round( ((geo_lat[1] - gacos_lat[1])) / geo_dy) - 1)
-        cutminlat_i = cutmaxlat_i + geo_ny
-        cutminlon_i = int(np.round( ((geo_lon[0] - gacos_lon[0])) / geo_dx) - 1)
-        cutmaxlon_i = cutminlon_i + geo_nx
-        gacos_ztd_geo = gacos_ztd_res[cutmaxlat_i:cutminlat_i, cutminlon_i:cutmaxlon_i]
-        
         # save output...
-        print('Saving GACOS correction in NSBAS geocoded geometry to: {}'.format(gacos_dir+'/'+str(int_date)+'.ztd.geo'))
-        gacos_ztd_geo.astype(np.float32).tofile(gacos_dir+'/'+str(int_date)+'.ztd.geo')
-        
-        # copy across the lookup table rsc file to be used as rsc file for the  gacos.ztd.geo
-        if os.path.isfile(gacos_dir+'/'+str(int_date)+'.ztd.geo.rsc') != True:
-            copyfile(lt_file+'.rsc', gacos_dir+'/'+str(int_date)+'.ztd.geo.rsc')
+        logger.info('Saving GACOS correction in real4 ROI_PAC format to: {}'.format(gacos_dir+'/'+str(int_date)+'.ztd.geo'))
+        gacos_ztd.astype(np.float32).tofile(outfile)
+        # create rsc file
+        f = open(outfile+'.rsc', "w")
+        f.write("""\
+WIDTH                 %d
+FILE_LENGTH           %d
+X_FIRST               %f
+Y_FIRST               %f
+X_STEP                %.8f
+Y_STEP                %.8f
+        """ % (ds.RasterXSize, ds.RasterYSize, ds_geo[0], ds_geo[3], ds_geo[1], ds_geo[5]))
+        f.close()
+
         
     elif os.path.isfile(gacos_dir+'/'+str(int_date)+'.ztd.geo') == True:
-        print('GACOS geo file for {} already exists. Must delete first to re-generate.'.format(int_date))
+        print('GACOS geo file for {} already exists. Use force mode.'.format(int_date))
         
 def gacosgeo2rdr(int_date):
     # in file, ztd cropped and resampled to geo
@@ -189,27 +184,35 @@ def gacosgeo2rdr(int_date):
     
     # out file for saving ztd in rdr
     gacos_rdr_file = gacos_dir + '/' + str(int_date) + '.ztd.rdr'
-    
+   
+    if force:
+        rm(gacos_rdr_file)
+
     if os.path.exists(gacos_rdr_file) != True:
-        print('Generating GACOS correction in NSBAS radar geometry: {}'.format(gacos_dir+'/'+str(int_date)+'.ztd.rdr'))
+        logger.info('Generating GACOS correction in NSBAS radar geometry: {}'.format(gacos_dir+'/'+str(int_date)+'.ztd.rdr'))
         # calling roipac perl script for geocoding
+        logger.info("geo2radar.pl %s %s %s %s" %(lt_file, gacos_geo_file, sim_dem_rdr, gacos_rdr_file))
         subprocess.call("geo2radar.pl %s %s %s %s" %(lt_file, gacos_geo_file, sim_dem_rdr, gacos_rdr_file), shell=True)
+        copyrsc(sim_dem_rdr+'.rsc',gacos_rdr_file+'.rsc')
         if os.stat(gacos_rdr_file).st_size == 0:
             #raise Exception('Warning: Output radar geometry GACOS correction is size zero.')
-            print('Warning: Output radar geometry GACOS correction is size zero.')
+            logger.critical('Output radar geometry GACOS correction is size zero.')
             print(gacos_rdr_file)
             sys.exit()
     else:
-        print('GACOS rdr file for {} already exists. Must delete *.rdr *geo.hst first to re-generate.'.format(int_date))
+        logger.info('GACOS rdr file for {} already exists. Use force mode.'.format(int_date))
 
 def gacosztd2los(int_date):
-    print('Converting GACOS in ztd, to los geometry and radians.')
+    logger.info('Converting GACOS in ztd, to los geometry and radians.')
     
     # In file from gacosgeo2rdr
     gacos_ztd_file = gacos_dir + '/' + str(int_date) + '.ztd.rdr'
     # Out file in los 
     gacos_los_file = gacos_dir + '/' + str(int_date) + '.rdr'
     
+    if force:
+        rm(gacos_los_file)
+
     if os.path.exists(gacos_los_file) != True:
         # Import gacos ztd file
         gacos_ztd_data = np.fromfile(gacos_ztd_file, np.float32).reshape(rdr_ny,rdr_nx)
@@ -221,15 +224,18 @@ def gacosztd2los(int_date):
         gacos_los_data_rads[inc_rad==0] = 0
         # Save output
         gacos_los_data_rads.astype(np.float32).tofile(gacos_los_file)
+        copyrsc(sim_dem_rdr+'.rsc',gacos_los_file+'.rsc')
     else:
-        print('GACOS rdr file for {} already exists. Must delete *.rdr *geo.hst first to re-generate.'.format(int_date))
+        print('GACOS rdr file for {} already exists. Use force mode.'.format(int_date))
 
 def gacosrdr2png(int_date, plot='yes'):
     # out file of gacosgeo2rdr
     gacos_rdr_file = gacos_dir + '/' + str(int_date) + '.rdr'
-    
+   
+    if force:
+        rm(gacos_rdr_file+'.png')
     if plot=='yes' and os.path.exists(gacos_rdr_file+'.png')!=True:
-        print('Converting GACOS in radar geometry to PNG format: {}'.format(gacos_rdr_file+'.png'))
+        logger.info('Converting GACOS in radar geometry to PNG format: {}'.format(gacos_rdr_file+'.png'))
         # plot png of rdr gacos corrections for checking
         gacos_rdr_data = np.fromfile(gacos_rdr_file, np.float32).reshape((rdr_ny,rdr_nx))
         
@@ -255,9 +261,6 @@ def gacosrdr2png(int_date, plot='yes'):
 # read input parameters and arguments
 arguments = docopt.docopt(__doc__)
 
-if arguments["--geo_rsc"] != None:
-    geo_rsc_file = arguments["--geo_rsc"]
-
 if arguments["--rdr_rsc"] != None:
     rdr_rsc_file = arguments["--rdr_rsc"]
     
@@ -280,16 +283,24 @@ if arguments["--nproc"] == None:
     nproc = 1
 else:
     nproc = int(arguments["--nproc"])
-    
+if arguments["-f"]:
+    force = True
+else:
+    force = False
+
 #####################################################################################
 # INITIALISE 
 ######################################################################################
 
 if __name__ == '__main__':
-    
-    print('NSBAS/ROI_PAC must be loaded prior to using this script.')
-    print('If using multi-processing and crashing, try (bash): unset DISPLAY')
-    print('Some issues with multiprocessing (parallel writes) with geo2radar.pl; try setting --nproc=1')
+   
+    logging.basicConfig(level=logging.INFO,\
+        format='line %(lineno)s -- %(levelname)s -- %(message)s')
+    logger = logging.getLogger('gacos2nsbrdr.log')
+
+    logger.info('NSBAS/ROI_PAC must be loaded prior to using this script.')
+    logger.info('If using multi-processing and crashing, try (bash): unset DISPLAY')
+    logger.info('Some issues with multiprocessing (parallel writes) with geo2radar.pl; try setting --nproc=1')
 
     # read in radar geometry parameters
     rdr_nx, rdr_ny, rdr_dict = rdr_rsc(rdr_rsc_file, full=True)
@@ -297,15 +308,18 @@ if __name__ == '__main__':
     rdr_rad2mm = (rdr_wl/(4*const.pi))*1000
     
     # incidence file for S1 TOPS (nsbas, in degrees)
-    inc_head = np.fromfile(inc_file, np.float32)[:rdr_ny*rdr_nx*2].reshape(rdr_ny, rdr_nx*2)
-    inc = inc_head[:,:rdr_nx]
+    ds = gdal.Open(inc_file, gdal.GA_ReadOnly)
+    inc = ds.GetRasterBand(2).ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize)[:rdr_ny,:rdr_nx]
     inc_rad = np.deg2rad(inc)
 
     # read in geocoded geometry parameters
-    geo_lon, geo_lat, geo_nx, geo_ny, geo_dict = geo_rsc(geo_rsc_file, full=True, verbose=False)
-    geo_dx = np.float(geo_dict['X_STEP'])
-    geo_dy = np.float(geo_dict['Y_STEP'])
-
+    ds = gdal.Open(lt_file, gdal.GA_ReadOnly)
+    ds_geo = ds.GetGeoTransform()
+    geo_dx, geo_dy = ds.GetGeoTransform()[1], ds.GetGeoTransform()[5]
+    geo_nx, geo_ny = ds.RasterXSize, ds.RasterYSize
+    geo_lon = ds_geo[0], ds_geo[0]+ ds_geo[1]*ds.RasterXSize
+    geo_lat = ds_geo[3]+ds_geo[5]*ds.RasterYSize, ds_geo[3] 
+    
     # extract interferogram date list from int pairs
     date1_list = np.loadtxt(int_list, unpack=True, dtype='i')[0]
     date2_list = np.loadtxt(int_list, unpack=True, dtype='i')[1]
@@ -317,10 +331,14 @@ if __name__ == '__main__':
     for int_date in int_date_list:
         if os.path.exists(gacos_dir + '/' + str(int_date) + '.ztd') != True:
             raise Exception('No GACOS correction in {} for {} in {}.'.format(gacos_dir,int_date,int_list))
+    
+    # Convert gacos model to tiff format of the  same size than geomap.trans
+    with Pool(processes=nproc) as pool:
+        results = pool.starmap(convert, list(zip(int_date_list)))
 
     # Run gacos2geo with a pool of agents (nproc)
-    with Pool(processes=nproc) as pool:
-        results = pool.starmap(gacos2geo, list(zip(int_date_list)))
+    #with Pool(processes=nproc) as pool:
+    #    results = pool.starmap(gacos2geo, list(zip(int_date_list)))
         
     # Run gacosgeo2rdr with a pool of agents (nproc)
     with Pool(processes=nproc) as pool:
@@ -331,5 +349,5 @@ if __name__ == '__main__':
         results = pool.starmap(gacosztd2los, list(zip(int_date_list)))
         
     # Run gacosrdr2png with a pool of agents (nproc)
-    with Pool(processes=nproc) as pool:
-        results = pool.starmap(gacosrdr2png, list(zip(int_date_list)))
+    #with Pool(processes=nproc) as pool:
+    #results = pool.starmap(gacosrdr2png, list(zip(int_date_list)))
