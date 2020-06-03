8f05840d3acd8f38d322955a2e183305e6ad1e5d
Nicholas Dodds <nick_dodds@hotmail.co.uk>
2020-06-03 15:27:08 +0100

    Updated GACOScorr script: STD reduction, histogram plotting

--- atmocorr/nsb_unw_GACOScorr_stats.py
+++ atmocorr/nsb_unw_GACOScorr_stats.py
@@ -77,6 +77,7 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 ### Load colormaps
 cm_locs = '/home/comethome/jdd/ScientificColourMaps5/by_platform/python/'
 cmap = LinearSegmentedColormap.from_list('roma', np.loadtxt(cm_locs+"roma.txt")).reversed()
+seq_cmap = LinearSegmentedColormap.from_list('', np.loadtxt(cm_locs+"lajolla.txt"))
 
 ### Functions
 
@@ -93,10 +94,10 @@ def rdr_rsc(inname, full=False, verbose=False):
                 .. note:: 
                         Currently set up to work with dem.rsc file from ROI-PAC.
             '''
-
+            
         if verbose:
-                logger.info("PROGRESS: READING %s RSC FILE" %inname)
-
+            logger.info("PROGRESS: READING %s RSC FILE" %inname)
+        
         rpacdict = {}
         infile = open(inname,'r')
         line = infile.readline()
@@ -156,7 +157,6 @@ def ramp_fit_int_gacos(rms_map, i_data, g_data):
     print ('Remove ramp  %f az  + %f r + %f.'%(a, b, c))
     
     return pars
-
 
 def sliding_median(x_data,y_data):
     perc = 90
@@ -274,17 +274,20 @@ def correct_int_gacos_recons(m_date, s_date, ramp_pars, ramp_pars_recons, maps='
     g_data[g_data == 0] = 0.
     ig_corr = ig_corr -  ig_data_refz_cst
     
+    # Calculate standard deviation reduction between input IFG and IFG corr
+    std_red = ((np.nanstd(i_data) - np.nanstd(ig_corr_recons))/np.nanstd(i_data))*100
+    
     # Save output in the output gacos int directory
     if not os.path.exists(int_gdir+'/'+int_did):
         os.mkdir(int_gdir+'/'+int_did)
     
     if maps=='yes':
         # Plot phase maps of ifg, gacos, ramp, ifg - gacos - ramp, ramp_recons, ifg - gacos - ramp_recons
-        maps_phase_gacos_ramp_corr_recons(int_did, m_date, s_date, i_data, g_data + ramp_res, ig_corr, ramp_res, ramp_res_recons, ig_corr_recons, c_data)
+        maps_phase_gacos_ramp_corr_recons(int_did, m_date, s_date, i_data, g_data, i_data-g_data, ramp_res, ramp_res_recons, ig_corr_recons, c_data)
     
     if phase_gacos=='yes':
         # Plot IFG phase vs GACOS+RAMP (return correlation and gradient)
-        ig_gradient, ig_correlation = phase_vs_gacos(int_did, m_date, s_date, i_data_recons-ramp_res_recons, g_data_recons, c_data, dem_data)
+        ig_gradient, ig_correlation = phase_vs_gacos(int_did, m_date, s_date, i_data_recons-ramp_res_recons, g_data_recons, c_data, dem_data, std_red)
     
     # Set areas outside = 0 using c_data (amplitude)
     ig_corr_recons[c_data==0] = 0
@@ -305,7 +308,7 @@ def correct_int_gacos_recons(m_date, s_date, ramp_pars, ramp_pars_recons, maps='
     # Caculate standard deviation for inversion weighting...
     g_std = np.nanstd(g_data + ramp_res_recons)
     
-    return m_date, s_date, ig_gradient, ig_correlation, g_std
+    return m_date, s_date, ig_gradient, ig_correlation, g_std, std_red
 
 def maps_phase_gacos_ramp_corr_recons(int_did, m_date, s_date, map1, map2, map3, map4, map5, map6, map_mask):
     logger.info('Plotting IFG - GACOS - RAMP = IFGcorr phase maps: {}_{}'.format(m_date, s_date))
@@ -378,7 +381,7 @@ def maps_phase_gacos_ramp_corr_recons(int_did, m_date, s_date, map1, map2, map3,
     divider = make_axes_locatable(ax2)
     c = divider.append_axes("right", size="5%", pad=0.05)
     plt.colorbar(cax2, cax=c)
-    ax2.set_title('$\phi_{GACOS} + \phi_{RAMP}$: '+str(m_date)+'_'+str(s_date))
+    ax2.set_title('$\phi_{GACOS}$: '+str(m_date)+'_'+str(s_date))
     
     # Plotting the raw unw IFG data
     ax3 = fig.add_subplot(2,3,3)
@@ -391,7 +394,7 @@ def maps_phase_gacos_ramp_corr_recons(int_did, m_date, s_date, map1, map2, map3,
     divider = make_axes_locatable(ax3)
     c = divider.append_axes("right", size="5%", pad=0.05)
     plt.colorbar(cax3, cax=c)
-    ax3.set_title('$\phi_{IFG} - \phi_{GACOS} - \phi_{RAMP}$: '+str(m_date)+'_'+str(s_date))
+    ax3.set_title('$\phi_{IFG} - \phi_{GACOS}$: '+str(m_date)+'_'+str(s_date))
     
     # Plotting the raw unw IFG data
     ax1 = fig.add_subplot(2,3,4)
@@ -439,7 +442,7 @@ def maps_phase_gacos_ramp_corr_recons(int_did, m_date, s_date, map1, map2, map3,
     
     plt.clf()
     
-def phase_vs_gacos(int_did, m_date, s_date, i_data, g_data, c_data, dem_data):
+def phase_vs_gacos(int_did, m_date, s_date, i_data, g_data, c_data, dem_data, std_red):
     logger.info('Generating IFG phase vs GACOS phase estimation: {}_{}'.format(m_date, s_date))
     
     i_data = deepcopy(i_data)
@@ -539,8 +542,8 @@ def phase_vs_gacos(int_did, m_date, s_date, i_data, g_data, c_data, dem_data):
     ax1.set_ylabel('$\phi_{GACOS}$: '+str(m_date)+'_'+str(s_date))
     
     # scatter labels to fine for legend so...
-    labels = ['Gradient: {:.2f}'.format(popt[0]), 'Correlation: {:.2f}'.format(rvalue)]
-    label_colours = [(0,0,0,0),(0,0,0,0)]
+    labels = ['Gradient: {:.2f}'.format(popt[0]), 'Correlation: {:.2f}'.format(rvalue), 'STD Reduction: {:.1f}%'.format(std_red)]
+    label_colours = [(0,0,0,0),(0,0,0,0),(0,0,0,0)]
     recs = []
     for i in range(0,len(label_colours)):
         recs.append(patches.Rectangle((0,0),1,1,fc=label_colours[i]))
@@ -710,7 +713,7 @@ if __name__ == '__main__':
         ramp_results_array = np.concatenate((m_dates[:,None], s_dates[:,None], ramps_results), axis=1) 
         np.savetxt(int_gdir+'/phase-gacos_ramp_estim.txt', ramp_results_array, fmt='%i, %i, %.7f, %.7f, %.7f', delimiter=' ')
     else:
-        logger.info("Skipping ramp estimation (estim='no")
+        logger.info("Skipping ramp estimation (estim='no')")
     
     # Read in results from txt file saved above...
     ramps_results_txt = np.loadtxt(int_gdir+'/phase-gacos_ramp_estim.txt', delimiter=', ')
@@ -768,9 +771,9 @@ if __name__ == '__main__':
 
     logger.info('Saving results for histogram of IFG vs. GACOS (correlation and gradient)')
     
-    # Save results as text file: m_date, s_date, gradient, correlation, atmos_std
+    # Save results as text file: m_date, s_date, gradient, correlation, atmos_std, std_red (IFG-IFGcorr)
     results_array = np.asarray(results)
-    np.savetxt(int_gdir+'/phase-gacos_stats_estim.txt', results_array, fmt='%i, %i, %.3f, %.3f, %.3f', delimiter=' ')
+    np.savetxt(int_gdir+'/phase-gacos_stats_estim.txt', results_array, fmt='%i, %i, %.3f, %.3f, %.3f, %.3f', delimiter=' ')
     
     logger.info('Calculating inversion weighting file using correlation and atmos standard deviation.')
     
@@ -788,3 +791,48 @@ if __name__ == '__main__':
     sigma_corr_std_weights = np.concatenate((m_dates[:,None], s_dates[:,None], np.asarray(sigma_corr)[:,None], np.asarray(sigma_std)[:,None], np.asarray(sigma_weight)[:,None]), axis=1)
     np.savetxt(int_gdir+'/phase-gacos_sigma_corr_weights.txt', sigma_corr_std_weights, fmt='%i, %i, %.3f, %.3f, %.3f', delimiter=' ')
     
+    ### Generate various histograms for analysis of overall performance...
+    print('Generating histograms for atmospheric correction performance analysis...')
+    atmos_data = np.loadtxt(int_gdir+'/phase-gacos_stats_estim.txt', delimiter=',')
+    atmos_grads = np.asarray([i[2] for i in atmos_data])
+    atmos_corrs = np.asarray([i[3] for i in atmos_data])
+    atmos_stdreds = np.asarray([i[5] for i in atmos_data])
+    
+    # Plotting    
+    fig = plt.figure(constrained_layout=True, figsize=(22,5))
+    gs_ws = [1, 1, 1, 1.2]
+    gs_hs = [1]
+    spec = fig.add_gridspec(ncols=4, nrows=1, width_ratios=gs_ws, height_ratios=gs_hs)
+    ax1 = fig.add_subplot(spec[0, 0])
+    ax2 = fig.add_subplot(spec[0, 1])
+    ax3 = fig.add_subplot(spec[0, 2])
+    ax4 = fig.add_subplot(spec[0, 3])
+    
+    ## Gradients
+    sns.distplot(atmos_grads, norm_hist=True, hist=True, color="dodgerblue", ax=ax1)
+    ax1.set_xlabel(r'Gradient ($m$)')
+    ax1.set_ylabel('Norm. PDF')
+    ax1.set_ylim(bottom=0)
+    ## Correlations
+    sns.distplot(atmos_corrs, norm_hist=True, hist=True, color="lightcoral", ax=ax2)
+    ax2.set_xlabel(r'Correlation ($r$)')
+    ax2.set_ylabel('Norm. PDF')
+    ax2.set_ylim(bottom=0)
+    ## STD reduction
+    sns.distplot(atmos_stdreds, norm_hist=True, hist=True, color="darkseagreen", ax=ax3)
+    ax3.set_xlabel(r'Standard Deviation Reduction (%)')
+    ax3.set_ylabel('Norm. PDF')
+    ax3.set_ylim(bottom=0)
+    ## Scatter of gradients vs. correlation, colored by std reduction
+    points = ax4.scatter(atmos_grads, atmos_corrs, c=atmos_stdreds, s=50, cmap=seq_cmap, edgecolor='black')
+    divider = make_axes_locatable(ax4)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = plt.colorbar(points, cax=cax)
+    cbar.set_label(r'Standard Deviation Reduction (%)', rotation=270, labelpad=15)
+    ax4.set_xlabel(r'Gradient ($m$)')
+    ax4.set_ylabel('Correlation ($r$)')
+    ##
+    fig.savefig(int_gdir+'/atmoscorr_histos.png', dpi=300, format='PNG', bbox_inches='tight')
+    plt.clf()
+    
+# program end
