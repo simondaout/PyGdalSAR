bf44fe1a23914646e27e27e25bc68591a710ec1d
Simon Daout <daouts@ist-oar.u-ga.fr>
2022-02-02 23:07:37 +0100

    script to replace rmg2grd.pl

--- utils/rmg2grd.py
+++ utils/rmg2grd.py
@@ -24,16 +24,34 @@ Options:
 --infile PATH       Input complex file
 """
 
-import os, math
+import os, math, sys
 import numpy as np
 from osgeo import gdal
 gdal.UseExceptions()
 # docopt (command line parser)
 import docopt
 
+def checkinfile(file):
+  if os.path.exists(file) is False:
+    print("File: {0} not found, Exit!".format(file))
+    print("File: {0} not found in {1}, Exit!".format(file,os.getcwd()))
+    sys.exit()
+
+def CreateOutfile(outfile, ds ,band):
+  driver = gdal.GetDriverByName("NetCDF")
+  dst_ds = driver.Create(outfile, ds.RasterXSize, ds.RasterYSize, 1, gdal.GDT_Float32)
+  dst_band1 = dst_ds.GetRasterBand(1)
+  gt = ds.GetGeoTransform()
+  proj = ds.GetProjection()
+  dst_band1.WriteArray(band,0,0)
+  dst_ds.SetGeoTransform(gt)
+  dst_ds.SetProjection(proj)
+  dst_band1.FlushCache()
+  del dst_ds 
+
 # read arguments
 arguments = docopt.docopt(__doc__)
-infile = arguments["<infile>"]
+infile = arguments["<infile>"]; checkinfile(infile)
 extension = os.path.splitext(infile)[1]
 basename = os.path.splitext(infile)[0]
 rsc = arguments["<infile>"] + '.rsc' 
@@ -41,55 +59,30 @@ rsc = arguments["<infile>"] + '.rsc'
 try:
     import nsb_utilproc
     wavelength = nsb_utilproc.get_value_rscfile(rsc, "WAVELENGTH")
-    phs_factor = wavelength / (4*math.pi)
 except:
-    phs_factor = 1
+    print('Cannot read WAVELENGTH in rsc file. Set WAVELENGTH to 0.05546576')
+    wavelength = 0.05546576
+phs_factor = float(wavelength) / (4*math.pi)
 
 # Open phiset (image)
-ds = gdal.OpenEx(infile, allowed_drivers=["ROI_PAC"])
+ds = gdal.OpenEx(infile, nOpenFlags=gdal.GA_ReadOnly, allowed_drivers=["ROI_PAC"])
 ds_band1 = ds.GetRasterBand(1)
 ds_band2 = ds.GetRasterBand(2)
 # Attributes
 print("> Driver:   ", ds.GetDriver().ShortName)
 print("> Size:     ", ds.RasterXSize,'x',ds.RasterYSize,'x',ds.RasterCount)
 print("> Datatype: ", gdal.GetDataTypeName(ds_band1.DataType))
-gt = ds.GetGeoTransform()
-proj = ds.GetProjection()
 
 # read and convert NaN
 pha = ds_band2.ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize)
-kk = np.flatnonzero(np.logical_or(pha>9990, pha==0.))
+kk = np.nonzero(np.logical_or(pha>9990, pha==0.))
 pha[kk] = float('NaN')
 mag = ds_band1.ReadAsArray(0, 0, ds.RasterXSize, ds.RasterYSize)
+ds_band1.FlushCache; ds_band2.FlushCache
 
-outfile = basename + '.mag.grd'
-driver = gdal.GetDriverByName("NetCDF")
-dst_ds = driver.Create(outfile, ds.RasterXSize, ds.RasterYSize, 1, gdal.GDT_Float32)
-dst_band1 = dst_ds.GetRasterBand(1)
-dst_band1.WriteArray(mag,0,0)
-dst_ds.SetGeoTransform(gt)
-dst_ds.SetProjection(proj)
-dst_band1.FlushCache()
-del dst_ds 
+outfile = basename + '.mag.grd'; CreateOutfile(outfile, ds, mag)
+outfile = basename + '.phs.grd'; CreateOutfile(outfile, ds, pha)
+outfile = basename + '.los.grd'; CreateOutfile(outfile, ds, pha*phs_factor)
 
-outfile = basename + '.phs.grd'
-driver = gdal.GetDriverByName("NetCDF")
-dst_ds = driver.Create(outfile, ds.RasterXSize, ds.RasterYSize, 1, gdal.GDT_Float32)
-dst_band1 = dst_ds.GetRasterBand(1)
-dst_band1.WriteArray(pha,0,0)
-dst_ds.SetGeoTransform(gt)
-dst_ds.SetProjection(proj)
-dst_band1.FlushCache()
-del dst_ds
-
-outfile = basename + '_nan_mmyr.grd'
-driver = gdal.GetDriverByName("NetCDF")
-dst_ds = driver.Create(outfile, ds.RasterXSize, ds.RasterYSize, 1, gdal.GDT_Float32)
-dst_band1 = dst_ds.GetRasterBand(1)
-dst_band1.WriteArray(pha*phs_factor,0,0)
-dst_ds.SetGeoTransform(gt)
-dst_ds.SetProjection(proj)
-dst_band1.FlushCache()
-del dst_ds, ds
-
+del ds
 
