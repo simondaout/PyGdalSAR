3a6941c17983faa9fa317160570dbc4c4df431d4
simondaout <simondaout@gmail.com>
2024-01-25 13:21:17 +0100

    remove old scripts

--- AtmoCorr/fetch_era5.py
+++ AtmoCorr/fetch_era5.py
@@ -9,23 +9,48 @@
 # Author        : Simon DAOUT (Oxford)
 ############################################
 
-import ecmwfapi
+#import ecmwfapi
+#server = ecmwfapi.ECMWFDataServer()
 
-server = ecmwfapi.ECMWFDataServer()
-server.retrieve({
-    "class": "ea",
-    "dataset": "era5",
-    "date": "2007-01-01/to/2010-12-31", # Time period
-    "expver": "1",
-    "levtype": "pl",
-    "levelist": "850/700/600/500",
-    "param": "130",           # Parameters. Here we use 2m Temperature (2t) and Surface Pressure (sp). See the ECMWF parameter database, at http://apps.ecmwf.int/codes/grib/param-db
-    "stream": "oper",
-    "type": "an",
-    "time": "0/12",
-    "step": "0",
-    "area": "27.25/89.00/28.25/90.00",    # Subset or clip to an area, here to Europe. Specify as North/West/South/East in Geographic lat/long degrees. Southern latitudes and Western longitudes must be given as negative numbers.
-    "grid": "0.25/0.25",        # Regrid from the default grid to a regular lat/lon with specified resolution. The first number is east-west resolution (longitude) and the second is north-south (latitude).
-    "format": "netcdf",         # Convert the output file from the default GRIB format to NetCDF format. Requires "grid" to be set to a regular lat/lon grid.
-    "target": "data.nc",    # The output file name. Set this to whatever you like.
-})
+import cdsapi
+server = cdsapi.Client()
+
+server.retrieve(
+    'reanalysis-era5-single-levels',        
+    {
+        'product_type':'reanalysis',
+        'variable':'2m_temperature',
+        'year':'2018',
+        'month':[
+            '01','02','03',
+            '04','05','06',
+            '07','08','09',
+            '10','11','12'
+        ],
+        'day':[
+            '01','02','03',
+            '04','05','06',
+            '07','08','09',
+            '10','11','12',
+            '13','14','15',
+            '16','17','18',
+            '19','20','21',
+            '22','23','24',
+            '25','26','27',
+            '28','29','30',
+            '31'
+        ],
+        "time": [
+            '00:00','01:00','02:00',
+            '03:00','04:00','05:00',
+            '06:00','07:00','08:00',
+            '09:00','10:00','11:00',
+            '12:00','13:00','14:00',
+            '15:00','16:00','17:00',
+            '18:00','19:00','20:00',
+            '21:00','22:00','23:00'
+        ],
+        "area": "38.5/96.5/38.0/97.",    # Subset or clip to an area, here to Europe. Specify as North/West/South/East in Geographic lat/long degrees. Southern latitudes and Western longitudes must be given as negative numbers.
+        "format": 'netcdf',
+        },
+    'data.nc')
