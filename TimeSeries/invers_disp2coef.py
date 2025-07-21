707d60ef3d4eb8732beb927ea3a9c323e885b263
simondaout <simondaout@gmail.com>
2025-07-21 18:18:47 +0200

    correct linear_inv

--- TimeSeries/invers_disp2coef.py
+++ TimeSeries/invers_disp2coef.py
@@ -1153,12 +1153,15 @@ def consInvert(A,b,sigmad,ineq='yes',cond=1.0e-3, iter=100,acc=1e-6, eguality=Fa
     return fsoln,sigmam
 
 
-def linear_inv(G, data, sigmad):
+def linear_inv(G, data, sigma):
       'Iterative linear inversion'
 
-      W = np.diag(1.0 / sigmad)
-      x0 = lst.lstsq(W @ G, W @ data, rcond=None)[0]
-      return x0
+      x0 = lst.lstsq(G,data)[0]
+      _func = lambda x: np.sum(((np.dot(G,x)-data)/sigma)**2)
+      _fprime = lambda x: 2*np.dot(G.T/sigma, (np.dot(G,x)-data)/sigma)
+      pars = opt.fmin_slsqp(_func,x0,fprime=_fprime,iter=2000,full_output=True,iprint=0,acc=1.e-9)[0]
+
+      return pars
 
 def estim_ramp(los,los_clean,topo_clean,az,rg,order,sigma,nfit,ivar,l,ax_dphi):
       'Ramp/Topo estimation and correction. Estimation is performed on sliding median'
